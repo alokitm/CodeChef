69dfc084bc8fbc8c7b33bc65e858c44ee2f2970c
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-18 19:12:04 +0530

    Implement double rent calculation in Double Rent.cpp
    Fix syntax errors and implement double rent calculation.

---
+++ Double Rent.cpp
@@ -0,0 +1,8 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+#include<bits/stdc++.h>using namespace std;
+int main() f
+1/ your code goes here int X;
+cin >>X；
+cout <<2*x；
+  return 0;
+}
