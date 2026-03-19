943999bb68bd7cf4a74fcdff3deab294a8c00bbd
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-19 22:13:18 +0530

    Update and rename README.md to Masterchef finals.cpp

---
+++ Masterchef finals.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+#include <bits/stdc++.h>using namespace std;
+int main() f
+1/ your code goes here int T；
+cin >> T;
+while(T--)f
+int X;
+cin >>X；
+if(1<=X 8& X<=10)9
+cout<< "Yes" << endl;}
+elsef
+cout ^^  No" ^^ endl;}}
+return 0;
+  }
