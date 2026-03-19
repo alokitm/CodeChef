41662ae616d105ac1e0a66cd6b3ae35bcfb61065
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-19 22:05:40 +0530

    Correct syntax and formatting in Double Rent.cpp
    Fix syntax errors and improve formatting in Double Rent.cpp.

--- Double Rent.cpp
+++ Double Rent.cpp
@@ -1,6 +1,6 @@
 //https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
-#include<bits/stdc++.h>using namespace std;
-int main() f
+#includeusing namespace std;
+int main) f
 1/ your code goes here int X;
 cin >>X；
 cout <<2*x；
