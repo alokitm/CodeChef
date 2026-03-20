b49d9001155e612ae609418e91e68562e83c70dd
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-20 22:17:00 +0530

    Add Biryani classes program for multiplication

---
+++ Biryani classes.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X, Y ;
+	    cin >> X ;
+	    cin >> Y ;
+	    cout << X*Y << endl;
+	}
+	return 0 ;
+
+}
+
+
