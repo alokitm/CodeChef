eab51c551b78b691bf27dea06be7031ee44cfa63
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-24 08:58:17 +0530

    Add tax savings calculation program
    Implement a program to calculate tax savings based on user input.

---
+++ Saving Taxes.cpp
@@ -0,0 +1,16 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ; 
+	for(int i = 0; i < T; i++) {
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << X - Y << endl;
+	}
+	return 0 ;
+}
