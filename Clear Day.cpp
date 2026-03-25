54ba758def1d55d2fe97a49507e7be51b90b85ab
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-25 20:04:52 +0530

    Enhance code readability with comments
    Added comments to clarify variable purpose and code logic.

--- Clear Day.cpp
+++ Clear Day.cpp
@@ -1,12 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int X,Y;
-	cin >> X ;
-	cin >> Y ; 
-	cout << 7-(X+Y) ; 
-	return 0 ;
+	// Variables:
+	// X = number of days Chef worked
+	// Y = number of days spent on other activities
+	int X, Y;
+
+	// Input values
+	cin >> X;
+	cin >> Y;
+
+	// Total days in a week = 7
+	// Remaining free days = 7 - (X + Y)
+	cout << 7 - (X + Y);
+
+	return 0;
 }
