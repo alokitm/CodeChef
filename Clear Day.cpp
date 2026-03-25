32b58b7db2d3feabb1399667becd18f4c1f6fab5
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-25 20:02:51 +0530

    Add Clear Day solution in Clear Day.cpp
    Implement basic input and output for Clear Day problem.

---
+++ Clear Day.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin >> X ;
+	cin >> Y ; 
+	cout << 7-(X+Y) ; 
+	return 0 ;
+}
