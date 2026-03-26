48fecaf0b174c61ac7f8559d95b32383d737efe9
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-26 11:06:02 +0530

    Implement Ludo game logic for input handling

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUDO
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ; 
+	cin >> T ;
+	while(T--){
+	    int X ;
+	    cin >> X ;
+	    if(X==6){
+	        cout << "Yes" << endl;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }     
+	}
+	return 0 ;
+
+}
