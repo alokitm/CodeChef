06cc6972fbeeb645764b5bc84df4bcfa0bf9d8ef
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-28 20:03:07 +0530

    Fix syntax errors and improve code formatting

--- Masterchef finals.cpp
+++ Masterchef finals.cpp
@@ -1,14 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
-#include <bits/stdc++.h>using namespace std;
-int main() f
-1/ your code goes here int T；
-cin >> T;
-while(T--)f
-int X;
-cin >>X；
-if(1<=X 8& X<=10)9
-cout<< "Yes" << endl;}
-elsef
-cout ^^  No" ^^ endl;}}
-return 0;
-  }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = rank
+	    int X;
+	    cin >> X;
+
+	    // Check if rank is within top 10
+	    if (1 <= X && X <= 10) {
+	        cout << "Yes" << endl;
+	    }
+	    else {
+	        cout << "No" << endl;
+	    }
+	}
+
+	return 0;
+}
