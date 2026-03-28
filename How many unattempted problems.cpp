bb77a55293a959b81f6f6f6ceb31256408084a0c
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-28 20:00:23 +0530

    Implement calculation for unattempted problems
    Added basic implementation to calculate unattempted problems.

---
+++ How many unattempted problems.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y; 
+	cin >> X >> Y ;
+	cout << X-Y ;
+	return 0 ;
+	
+
+}
