5e042c8e4c69407b9005e5a7b1347e9d2b163645
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-28 20:02:11 +0530

    Improve code structure and add comments
    Refactor code for clarity and add comments.

--- How many unattempted problems.cpp
+++ How many unattempted problems.cpp
@@ -1,13 +1,19 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int X,Y; 
-	cin >> X >> Y ;
-	cout << X-Y ;
-	return 0 ;
-	
+	// X = total problems
+	// Y = solved problems
+	int X, Y;
 
+	// Input
+	cin >> X >> Y;
+
+	// Remaining problems
+	cout << X - Y;
+
+	return 0;
 }
