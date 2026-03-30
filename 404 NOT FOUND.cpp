0e4de63d46dd42c18a899de463ba71708196eee5
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-30 08:35:22 +0530

    Add program to check for error code 404

---
+++ 404 NOT FOUND.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X == 404)
+        cout << "NOT FOUND";
+    else
+        cout << "FOUND";
+
+    return 0;
+}
