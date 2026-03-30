4d59f75052c8dc4764676f77fcc204347d79eff0
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-30 08:36:43 +0530

    Implement status code check for 404 error

--- 404 NOT FOUND.cpp
+++ 404 NOT FOUND.cpp
@@ -1,11 +1,17 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // X = status code
     int X;
+
+    // Input
     cin >> X;
 
+    // Check if status code is 404
     if (X == 404)
         cout << "NOT FOUND";
     else
