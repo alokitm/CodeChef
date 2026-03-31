df51e694b7b8c8a632def5a3ae8ab84880b3f223
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-31 11:47:03 +0530

    Refactor Off BY One.cpp for clarity and comments

--- Off BY One.cpp
+++ Off BY One.cpp
@@ -1,13 +1,20 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int A,B ;
-	cin >> A >> B;
-	cout << A*10 + B*10 + 1  ; 
-	return 0 ;
-	
+	// A = number of 10-unit items
+	// B = number of 10-unit items
+	int A, B;
 
+	// Input
+	cin >> A >> B;
+
+	// Calculation:
+	// Total = (A * 10) + (B * 10) + 1
+	cout << A * 10 + B * 10 + 1;
+
+	return 0;
 }
