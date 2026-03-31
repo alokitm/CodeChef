940334ed996aa90eb8a2088781eb923ec488d7c8
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-31 11:46:18 +0530

    Add header and modify output calculation
    Updated the code to include necessary headers and fixed output formatting.

---
+++ Off BY One.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int A,B ;
+	cin >> A >> B;
+	cout << A*10 + B*10 + 1  ; 
+	return 0 ;
+	
+
+}
