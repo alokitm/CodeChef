c3983eac432b5aa7eac39bba321cfbcde0b544df
Alokit Mishra <alokitmishra4@gmail.com>
2026-03-31 11:50:16 +0530

    Enhance comments in Kitchen Timings.cpp
    Added comments for clarity on input and logic.

--- Kitchen Timings.cpp
+++ Kitchen Timings.cpp
@@ -1,14 +1,23 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENTIME
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+	// t = number of test cases
 	int t, x, y;
-    cin >> t;
-    
-    while (t--) {
-        cin >> x >> y;
-        cout << y - x << endl;
-    }
 
+	// Input number of test cases
+	cin >> t;
+
+	while (t--) {
+	    // x = start time, y = end time
+	    cin >> x >> y;
+
+	    // Time spent in kitchen
+	    cout << y - x << endl;
+	}
+
+	return 0;
 }
