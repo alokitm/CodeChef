09422b61d35b4ef1968c2c3ac38f35017a81269f
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-01 18:30:02 +0530

    Implement audible range check for input values

---
+++ Audible Range.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ; 
+	cin  >> T ;
+	while(T--){
+	    int X; 
+	    cin >> X;
+	    if(X >= 67 && X <= 45000){
+	        cout << "Yes" << endl;
+	    }
+	    else{
+	        cout << "No" << endl;
+	    }
+	        
+	}
+	return 0 ;
+
+}
