50fc6fb8331161292cd982c83bf33be8af89826e
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-02 09:06:35 +0530

    Add basic input handling for Puzzle Hunt problem

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ;
+	    int N ;
+	    cin >> N;
+	    if (N>=6 && N<=8){
+	        cout << "yes" << endl ;
+	    }
+	    else{
+	        cout << "no" << endl ;
+	    }
+	return 0 ;
+
+}
