ee60e6b78660635ca59272af8bd9ca6738ef48ab
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-02 09:08:40 +0530

    Refactor input handling and condition checks

--- Puzzle Hunt.cpp
+++ Puzzle Hunt.cpp
@@ -1,19 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	    int N ;
-	    cin >> N;
-	    if (N>=6 && N<=8){
-	        cout << "yes" << endl ;
-	    }
-	    else{
-	        cout << "no" << endl ;
-	    }
-	return 0 ;
+	// T = number of test cases
+	int T;
+	cin >> T;
 
+	while (T--) {
+	    // N = number of puzzles solved
+	    int N;
+	    cin >> N;
+
+	    // Check if N is between 6 and 8 (inclusive)
+	    if (N >= 6 && N <= 8) {
+	        cout << "yes" << endl;
+	    }
+	    else {
+	        cout << "no" << endl;
+	    }
+	}
+
+	return 0;
 }
