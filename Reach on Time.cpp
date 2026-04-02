bf4bd5e8247bb9091c4ed9e55e6fc8a1df2f159d
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-02 09:05:13 +0530

    Refactor Reach on Time program for clarity

--- Reach on Time.cpp
+++ Reach on Time.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
-	cin  >> T ;
-	while(T--){
-	    int X ;
-	    cin >> X ;
-	    if(X>=30){
-	        cout << "Yes" << endl ;
+	cin >> T;
+
+	while (T--) {
+	    // X = time available
+	    int X;
+	    cin >> X;
+
+	    // Check if time is at least 30 minutes
+	    if (X >= 30) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
