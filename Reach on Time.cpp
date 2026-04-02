331ccae7f517805f37c7edcdb1e4f5dacb3d8d00
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-02 09:03:41 +0530

    Implement Reach on Time program logic

---
+++ Reach on Time.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin  >> T ;
+	while(T--){
+	    int X ;
+	    cin >> X ;
+	    if(X>=30){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+	return 0 ;
+
+}
