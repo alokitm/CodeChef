4e15aa362dc4ddb93d62eadf1d19f194aa4c1de4
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-04 23:13:04 +0530

    Create Who is taller!.cpp

---
+++ Who is taller!.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TALLER
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T; 
+	cin >> T ;
+	while(T--){
+	    int X,Y ;
+	    cin >> X >> Y ;
+	    if(X>Y){
+	        cout << "A" << endl ;	        
+	    }
+	    else{
+	        cout << "B" << endl ; 
+	    }
+	}
+	return 0;
+
+}
