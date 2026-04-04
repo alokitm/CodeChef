3f4f80752a358b73647fa43d5ddbcf5ba89729e4
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-04 23:13:54 +0530

    Update Who is taller!.cpp

--- Who is taller!.cpp
+++ Who is taller!.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TALLER
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T; 
-	cin >> T ;
-	while(T--){
-	    int X,Y ;
-	    cin >> X >> Y ;
-	    if(X>Y){
-	        cout << "A" << endl ;	        
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = height of A, Y = height of B
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Compare heights
+	    if (X > Y) {
+	        cout << "A" << endl;
 	    }
-	    else{
-	        cout << "B" << endl ; 
+	    else {
+	        cout << "B" << endl;
 	    }
 	}
-	return 0;
 
+	return 0;
 }
