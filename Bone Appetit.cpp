fff2ef3950257fed86cbb9d385fb4e12ca3e1577
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-04 23:12:02 +0530

    Refactor Bone Appetit.cpp for clarity and comments

--- Bone Appetit.cpp
+++ Bone Appetit.cpp
@@ -1,14 +1,24 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// N = number of items of type 1
+	// M = number of items of type 2
 	int N, M;
-	cin >> N >> M ;
-	int X , Y ;
-	cin >> X >> Y ; 
-	cout << N*X + M*Y ;
-	return 0 ;
 
+	// X = cost of type 1 item
+	// Y = cost of type 2 item
+	int X, Y;
+
+	// Input
+	cin >> N >> M;
+	cin >> X >> Y;
+
+	// Total cost calculation
+	cout << N * X + M * Y;
+
+	return 0;
 }
