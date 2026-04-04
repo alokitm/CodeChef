13504e859572be415e5b35c509e374001b3ea822
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-04 23:11:07 +0530

    Create Bone Appetit.cpp

---
+++ Bone Appetit.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N, M;
+	cin >> N >> M ;
+	int X , Y ;
+	cin >> X >> Y ; 
+	cout << N*X + M*Y ;
+	return 0 ;
+
+}
