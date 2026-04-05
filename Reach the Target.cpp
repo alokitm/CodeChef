1b9863e0e0ffb5472905a5ddfa64f378002066c9
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-05 22:09:16 +0530

    Create Reach the Target.cpp

---
+++ Reach the Target.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACHTARGET
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X,Y ;
+	    cin >> X >> Y;
+	    cout << X-Y << endl ;
+	}
+	return 0 ;
+
+}
