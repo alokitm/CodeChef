21322e3d1f9fe238b263dc8d07340d8cb95edf53
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-05 22:11:41 +0530

    Implement solution to find maximum of two numbers

---
+++ Best of Two.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ;
+	while(T--){
+	    int X, Y ;
+	    cin >> X >> Y ;
+	    cout << max(X,Y) << endl ;
+	}
+	return 0;
+
+}
