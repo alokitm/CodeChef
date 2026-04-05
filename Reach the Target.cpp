96d39f09a3bb2979b8ef2187601d44b8422b7d6f
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-05 22:10:37 +0530

    Refactor Reach the Target program structure

--- Reach the Target.cpp
+++ Reach the Target.cpp
@@ -1,16 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACHTARGET
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int X,Y ;
-	    cin >> X >> Y;
-	    cout << X-Y << endl ;
-	}
-	return 0 ;
 
+	while (T--) {
+	    // X = target score, Y = current score
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Points needed to reach target
+	    cout << X - Y << endl;
+	}
+
+	return 0;
 }
