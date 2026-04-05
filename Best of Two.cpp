748e87898738bc655d4403468e9808ce43780f47
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-05 22:12:18 +0530

    Update Best of Two.cpp

--- Best of Two.cpp
+++ Best of Two.cpp
@@ -1,16 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
-	    int X, Y ;
-	    cin >> X >> Y ;
-	    cout << max(X,Y) << endl ;
-	}
-	return 0;
+	// T = number of test cases
+	int T;
+	cin >> T;
 
+	while (T--) {
+	    // X and Y = scores
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Output the maximum of the two
+	    cout << max(X, Y) << endl;
+	}
+
+	return 0;
 }
