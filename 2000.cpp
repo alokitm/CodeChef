6ee5574a041cd514139ffc242edfbea7decc59eb
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-06 23:07:41 +0530

    Create 2000.cpp

---
+++ 2000.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N ;
+	cin >> N;
+	//while (N--)
+	cout << N*4 ;
+	return 0 ;
+
+}
