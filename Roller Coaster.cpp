7def17496123203e4afa3b92b1590c4843004350
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-06 23:10:00 +0530

    Update Roller Coaster.cpp

--- Roller Coaster.cpp
+++ Roller Coaster.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINHEIGHT
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
-	    int X,H;
-	    cin >> X >> H ;
-	    if(X>=H){
-	        cout << "Yes" << endl ;
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = current height, H = required height
+	    int X, H;
+	    cin >> X >> H;
+
+	    // Check if height requirement is met
+	    if (X >= H) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
