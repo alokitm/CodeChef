d392e01486bb107849e6541458d4319686866850
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-06 23:09:24 +0530

    Implement height check for roller coaster eligibility

---
+++ Roller Coaster.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINHEIGHT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ;
+	while(T--){
+	    int X,H;
+	    cin >> X >> H ;
+	    if(X>=H){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+	return 0 ;
+
+}
