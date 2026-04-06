cbf551f215e68e99a3dc9526312132b76fc474cc
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-06 23:08:14 +0530

    Update 2000.cpp

--- 2000.cpp
+++ 2000.cpp
@@ -1,13 +1,16 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int N ;
+	// N = number of years
+	int N;
 	cin >> N;
-	//while (N--)
-	cout << N*4 ;
-	return 0 ;
 
+	// Each year gives 4 units
+	cout << N * 4;
+
+	return 0;
 }
