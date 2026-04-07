d2218fd77d41e0cb5f29dc3362383cd3285b9892
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-07 23:37:41 +0530

    Add basic structure for Chef On Date problem

---
+++ Chef On Date.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHEFONDATE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ;
+	while(T--){
+	    int X, Y;
+	    cin >> X >> Y ;
+	    if(X>=Y){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+	return 0 ;
+
+}
