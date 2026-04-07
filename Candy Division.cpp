0d4a3e3851c2b28afc438bd1b30da7544c213697
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-07 23:36:44 +0530

    Refactor Candy Division code for clarity and structure

--- Candy Division.cpp
+++ Candy Division.cpp
@@ -1,23 +1,24 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CANDIVIDE
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
-	    int N ;
-	    cin >> N ;
-	    if(N%3 == 0){
-	        cout << "Yes" << endl ;
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // N = given number
+	    int N;
+	    cin >> N;
+
+	    // Check if N is divisible by 3
+	    if (N % 3 == 0) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
-	    
 	}
-	return 0 ;
 
-
+	return 0;
 }
