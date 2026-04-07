18e0c501bc6c0617709477d817154ccc4b546326
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-07 23:38:27 +0530

    Update Chef On Date.cpp

--- Chef On Date.cpp
+++ Chef On Date.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHEFONDATE
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = amount Chef has, Y = required amount
 	    int X, Y;
-	    cin >> X >> Y ;
-	    if(X>=Y){
-	        cout << "Yes" << endl ;
+	    cin >> X >> Y;
+
+	    // Check if Chef can afford the date
+	    if (X >= Y) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
