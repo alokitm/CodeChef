51e6c0b10e28c6d927adc5031ef547a1c58d334c
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-08 23:19:13 +0530

    Refactor parity check logic in Parity.cpp

--- Parity.cpp
+++ Parity.cpp
@@ -1,20 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
+
+	while (T--) {
+	    // N = given number
 	    int N;
 	    cin >> N;
-	    if(N%2 == 0){
-	        cout << "Yes" << endl ;
+
+	    // Check if N is even
+	    if (N % 2 == 0) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-return 0;
+
+	return 0;
 }
