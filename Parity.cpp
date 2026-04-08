f3e22aeaa56367e60c828dbbd985c1c324ff80ec
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-08 23:20:04 +0530

    Update Solution of Clear Day.cpp

--- Parity.cpp
+++ Parity.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+// CodeChef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
 
 #include <bits/stdc++.h>
 using namespace std;
