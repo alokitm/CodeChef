154ab9152364699bc09a2eb265efc91b1e1e4939
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-08 23:15:06 +0530

    Implement parity check for multiple test cases

---
+++ Parity.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int N;
+	    cin >> N;
+	    if(N%2 == 0){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+return 0;
+}
