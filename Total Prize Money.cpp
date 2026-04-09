9b1f99b5b6b0afe3b9a485c64648bb6ca5bba0f6
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-09 19:08:27 +0530

    Add Total Prize Money calculation program

---
+++ Total Prize Money.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X,Y;
+	    cin >> X >> Y;
+        cout << 10*X + 90*Y << endl ;
+	}
+return 0;
+}
