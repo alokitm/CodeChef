42edf6186c2632441dc34b2bc0f04e6330f8d054
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-10 21:58:32 +0530

    Refactor word counting logic in main function

--- Counting Words.cpp
+++ Counting Words.cpp
@@ -1,15 +1,23 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int M,N;
+
+	while (T--) {
+	    // M = number of rows
+	    // N = number of columns
+	    int M, N;
 	    cin >> M >> N;
-	        cout << M*N << endl ;
+
+	    // Total number of words
+	    cout << M * N << endl;
 	}
-return 0;
+
+	return 0;
 }
