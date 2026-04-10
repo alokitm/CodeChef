c88045c41c9c42230bbd1aba0b3af6c256143106
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-10 21:57:28 +0530

    Implement word counting logic in Counting Words.cpp

---
+++ Counting Words.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int M,N;
+	    cin >> M >> N;
+	        cout << M*N << endl ;
+	}
+return 0;
+}
