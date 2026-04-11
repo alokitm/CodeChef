26945bef8ea66371b0bad5e55fd95f455c86e56e
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-11 23:46:42 +0530

    Update solution in Battery Health.cpp

--- Battery Health.cpp
+++ Battery Health.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
 
 #include <bits/stdc++.h>
 using namespace std;
