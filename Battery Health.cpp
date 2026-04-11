2d739b96b0bebdb6caba5805650ba7c350010aaf
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-11 23:44:42 +0530

    Implement battery health check based on input value

---
+++ Battery Health.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X;
+	    cin >> X;
+	    if(X>=80){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+return 0;
+}
