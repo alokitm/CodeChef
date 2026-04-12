8990436826a173818ebed516f732f4ae2d76dd24
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-12 18:34:03 +0530

    Create Tom and Jerry Chase.cpp

---
+++ Tom and Jerry Chase.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X,Y;
+	    cin >> X >> Y;
+	    if(Y>X){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+return 0;
+}
