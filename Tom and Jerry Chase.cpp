dcf22b46a9745f65b429f9d52af55672bbbd66fb
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-12 18:35:13 +0530

    Refactor Tom and Jerry Chase.cpp for clarity

--- Tom and Jerry Chase.cpp
+++ Tom and Jerry Chase.cpp
@@ -1,20 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int X,Y;
+
+	while (T--) {
+	    // X = speed of Tom, Y = speed of Jerry
+	    int X, Y;
 	    cin >> X >> Y;
-	    if(Y>X){
-	        cout << "Yes" << endl ;
+
+	    // Check if Jerry is faster than Tom
+	    if (Y > X) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-return 0;
+
+	return 0;
 }
