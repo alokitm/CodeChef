fe00cef3d5cd9055b784681e90adf41d8af29979
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-13 22:40:48 +0530

    Update Ageing.cpp

--- Ageing.cpp
+++ Ageing.cpp
@@ -1,16 +1,27 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AGEING
+// Problem Link:
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int X;
-	    cin >> X;
-	        cout << X-10 << endl ;
 
+	while (T--) {
+	    // X = speed of Tom, Y = speed of Jerry
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Check if Jerry is faster than Tom
+	    if (Y > X) {
+	        cout << "Yes" << endl;
+	    }
+	    else {
+	        cout << "No" << endl;
+	    }
 	}
-return 0;
+
+	return 0;
 }
