c7d13d79ab5a21e79d5f086742c241f0b239807c
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-13 22:40:12 +0530

    Create Ageing.cpp

---
+++ Ageing.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AGEING
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X;
+	    cin >> X;
+	        cout << X-10 << endl ;
+
+	}
+return 0;
+}
