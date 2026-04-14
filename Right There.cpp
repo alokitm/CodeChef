d59259cd4c19d4ba72aed20f79f253efcfcca356
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-14 22:44:24 +0530

    Add basic structure for Right There problem solution

---
+++ Right There.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIGHTTHERE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int N,X;
+	    cin >> N >> X;
+	    if(N<=X){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	        
+
+	}
+return 0;
+}
