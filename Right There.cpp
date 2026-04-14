92a7efdfb1ed40e1673dabbaab69225d6196720f
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-14 22:45:25 +0530

    Update problem link in Right There.cpp

--- Right There.cpp
+++ Right There.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIGHTTHERE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIGHTTHERE
 
 #include <bits/stdc++.h>
 using namespace std;
