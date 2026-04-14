365df300950143f56ac9bb74fce04bdd304cb9a8
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-14 22:45:08 +0530

    Refactor main function for clarity and structure

--- Right There.cpp
+++ Right There.cpp
@@ -1,22 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIGHTTHERE
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int N,X;
-	    cin >> N >> X;
-	    if(N<=X){
-	        cout << "Yes" << endl ;
-	    }
-	    else{
-	        cout << "No" << endl ;
-	    }
-	        
 
+	while (T--) {
+	    // N = required distance, X = current position
+	    int N, X;
+	    cin >> N >> X;
+
+	    // Check if Chef can reach or has already reached
+	    if (N <= X) {
+	        cout << "Yes" << endl;
+	    }
+	    else {
+	        cout << "No" << endl;
+	    }
 	}
-return 0;
+
+	return 0;
 }
