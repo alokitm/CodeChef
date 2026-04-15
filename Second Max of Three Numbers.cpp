4389a31a8b15da23484e8c3805c755d33bb32810
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-15 23:00:30 +0530

    Improve code structure and comments in C++ program
    Refactor code for clarity and maintainability.

--- Second Max of Three Numbers.cpp
+++ Second Max of Three Numbers.cpp
@@ -1,15 +1,28 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SNDMAX
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // n = number of test cases
     int n;
     cin >> n;
-    while(n--){
+
+    while (n--) {
+        // Array to store 3 numbers
         int nums[3];
+
+        // Input
         cin >> nums[0] >> nums[1] >> nums[2];
-        // Sort the array. After sorting, nums[1] will hold the second-maximum element.
+
+        // Sort the array in ascending order
+        // After sorting:
+        // nums[0] = smallest
+        // nums[1] = second maximum (middle)
+        // nums[2] = largest
         sort(nums, nums + 3);
+
+        // Output second maximum
         cout << nums[1] << endl;
     }
+
+    return 0;
 }
