a49b34293183aedd14c6a1db05f462777e5f2034
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-16 22:09:36 +0530

    Refactor Bull or Bear game logic and comments

--- Bull or Bear.cpp
+++ Bull or Bear.cpp
@@ -1,24 +1,27 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BULLBEAR
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
-	    int X,Y ;
-	    cin >> X >> Y ;
-	    if(Y>X){
-	        cout << "Profit" << endl ;
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = cost price, Y = selling price
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Determine profit, loss, or neutral
+	    if (Y > X) {
+	        cout << "Profit" << endl;
 	    }
-	    else if(Y==X){
-	        cout << "Neutral" << endl ;
+	    else if (Y == X) {
+	        cout << "Neutral" << endl;
 	    }
-	    else{
-	        cout << "Loss" << endl ;
+	    else {
+	        cout << "Loss" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
