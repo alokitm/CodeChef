28c69364b121ef37d03cf56d44f3e31a0f121f98
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-17 23:13:28 +0530

    Update Four Tickets.cpp

--- Four Tickets.cpp
+++ Four Tickets.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FOURTICKETS
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = cost of one ticket
 	    int X;
 	    cin >> X;
-	    if(4*X<=1000){
-	        cout << "Yes" << endl ;
+
+	    // Check if total cost of 4 tickets is within budget (<= 1000)
+	    if (4 * X <= 1000) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
