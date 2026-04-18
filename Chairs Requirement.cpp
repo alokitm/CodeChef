f8edf46790cdfebce3befd6a416f5956541f5eb9
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-18 22:47:38 +0530

    Refactor chair requirement calculation logic

--- Chairs Requirement.cpp
+++ Chairs Requirement.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHAIRS_
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
-	    int X,Y ;
-	    cin >> X >> Y ;
-	    if(X-Y>0){
-	         cout << X-Y << endl ;
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = total chairs needed, Y = chairs already available
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Remaining chairs required (cannot be negative)
+	    if (X - Y > 0) {
+	        cout << X - Y << endl;
 	    }
-	    else{
-	        cout << "0" << endl ;
+	    else {
+	        cout << "0" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
