88ea59aaf57c9ae03e4a9192fc0356735ef5878f
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-18 22:47:53 +0530

    Update problem link format in Chairs Requirement.cpp

--- Chairs Requirement.cpp
+++ Chairs Requirement.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHAIRS_
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHAIRS_
 
 #include <bits/stdc++.h>
 using namespace std;
