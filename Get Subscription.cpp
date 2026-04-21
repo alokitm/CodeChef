c9e025d28a71d5ddb263d0e589c44e7de9ea2675
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-21 23:52:26 +0530

    Create Get Subscription.cpp

---
+++ Get Subscription.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SUBSCRIBE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--){
+	    int X;
+	    cin >> X;
+	    if(X>30){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+return 0;
+}
