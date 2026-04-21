6779cdb824dd217eb72a71f5f946b2db58b3f8e0
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-21 23:53:02 +0530

    Update Get Subscription.cpp

--- Get Subscription.cpp
+++ Get Subscription.cpp
@@ -1,20 +1,22 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SUBSCRIBE
+// Problem Link:
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DNATION
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int X;
-	    cin >> X;
-	    if(X>30){
-	        cout << "Yes" << endl ;
-	    }
-	    else{
-	        cout << "No" << endl ;
-	    }
+
+	while (T--) {
+	    // X = current donations, Y = required donations
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Additional donations needed
+	    cout << Y - X << endl;
 	}
-return 0;
+
+	return 0;
 }
