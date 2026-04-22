3c812855dce75d54be58f126c0c74025ac69aa00
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-22 23:38:55 +0530

    Update problem link format in Messi vs Ronaldo.cpp

--- Messi vs Ronaldo.cpp
+++ Messi vs Ronaldo.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MVR
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MVR
 
 #include <bits/stdc++.h>
 using namespace std;
