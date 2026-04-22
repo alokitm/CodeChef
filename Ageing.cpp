f72b54901307595f8e4c5f543d9f26e4eaff2979
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-22 23:40:10 +0530

    Update problem link in Ageing.cpp

--- Ageing.cpp
+++ Ageing.cpp
@@ -1,4 +1,4 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
 
 #include <bits/stdc++.h>
 using namespace std;
