c6dd16abf2ed0d4ed7737aab26376094ceffc20b
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-22 23:38:40 +0530

    Update Messi vs Ronaldo.cpp

--- Messi vs Ronaldo.cpp
+++ Messi vs Ronaldo.cpp
@@ -1,22 +1,30 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MVR
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int A,B,X,Y ;
-	cin >> A >> B >> X >> Y ;
-	int Messi, Ronaldo ;
-	Messi = 2*A + B ;
-	Ronaldo = 2*X + Y ;
-	if(Messi>Ronaldo){
-	    cout << "Messi" << endl ;
+	// A, B = goals and assists of Messi
+	// X, Y = goals and assists of Ronaldo
+	int A, B, X, Y;
+	cin >> A >> B >> X >> Y;
+
+	// Calculate scores
+	int Messi, Ronaldo;
+	Messi = 2 * A + B;
+	Ronaldo = 2 * X + Y;
+
+	// Compare performances
+	if (Messi > Ronaldo) {
+	    cout << "Messi" << endl;
 	}
-	else if(Messi==Ronaldo){
-	    cout << "Equal" << endl ;
+	else if (Messi == Ronaldo) {
+	    cout << "Equal" << endl;
 	}
-	else{
-	    cout << "Ronaldo" << endl ;
+	else {
+	    cout << "Ronaldo" << endl;
 	}
-	return 0 ;
+
+	return 0;
 }
