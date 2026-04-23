e94fc189698c72a954219089aad4d1b97f6fc680
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-23 23:12:55 +0530

    Update Waiting Time.cpp

--- Waiting Time.cpp
+++ Waiting Time.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WAITTIME
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WAITTIME
 
 #include <bits/stdc++.h>
 using namespace std;
