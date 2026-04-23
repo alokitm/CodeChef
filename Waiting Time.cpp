27228b924f062808d432b885b1732a05e1014e7f
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-23 23:12:43 +0530

    Update Waiting Time.cpp

--- Waiting Time.cpp
+++ Waiting Time.cpp
@@ -1,18 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WAITTIME
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
-	    int K,X ;
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // K = number of weeks, X = days already waited
+	    int K, X;
 	    cin >> K >> X;
 
-	        cout << 7*K-X << endl ;
-
+	    // Total days = 7 * K, remaining wait time
+	    cout << 7 * K - X << endl;
 	}
-	return 0 ;
 
+	return 0;
 }
