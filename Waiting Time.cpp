90f3f69f6fdfd28a55988cfa596bf8d43b223580
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-23 23:11:49 +0530

    Implement waiting time calculation program

---
+++ Waiting Time.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WAITTIME
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ;
+	while(T--){
+	    int K,X ;
+	    cin >> K >> X;
+
+	        cout << 7*K-X << endl ;
+
+	}
+	return 0 ;
+
+}
