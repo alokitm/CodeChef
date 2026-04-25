1df0c80202996bb01ce795e909a114714d460810
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-25 22:21:47 +0530

    Refactor code for clarity and structure

--- Just One More Episode.cpp
+++ Just One More Episode.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ONEMORE
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ;
-	cin >> T ;
-	while(T--){
+	// T = number of test cases
+	int T;
+	cin >> T;
+
+	while (T--) {
+	    // X = number of hours studied
 	    int X;
 	    cin >> X;
-	    if(X>24){
-	        cout << "Yes" << endl ;
+
+	    // Check if X exceeds 24 hours
+	    if (X > 24) {
+	        cout << "Yes" << endl;
 	    }
-	    else{
-	        cout << "No" << endl ;
+	    else {
+	        cout << "No" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
