90a84719d3aee962d1f933a0f82c2ea0e59979eb
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-25 22:20:27 +0530

    Implement logic for checking episode count

---
+++ Just One More Episode.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ONEMORE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T ;
+	cin >> T ;
+	while(T--){
+	    int X;
+	    cin >> X;
+	    if(X>24){
+	        cout << "Yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	}
+	return 0 ;
+
+}
