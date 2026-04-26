9bd1b4ce26be8476d4a3cfa019ddc5f38cbf6934
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-26 22:35:05 +0530

    Add basic structure for Mana Points program

---
+++ Mana Points.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANAPTS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T ;
+	while(T--){
+	    int X,Y ;
+	    cin >> X >> Y ;
+	    cout << Y/X << endl;
+	}
+	return 0 ;
+
+}
