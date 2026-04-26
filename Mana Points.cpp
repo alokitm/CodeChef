382144ddb926618c2d84e2cc47436509d946fd59
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-26 22:36:10 +0530

    Refactor code structure for clarity and readability

--- Mana Points.cpp
+++ Mana Points.cpp
@@ -1,16 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANAPTS
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
-	cin >> T ;
-	while(T--){
-	    int X,Y ;
-	    cin >> X >> Y ;
-	    cout << Y/X << endl;
-	}
-	return 0 ;
+	cin >> T;
 
+	while (T--) {
+	    // X = cost per apartment, Y = total budget
+	    int X, Y;
+	    cin >> X >> Y;
+
+	    // Maximum number of apartments that can be bought
+	    cout << Y / X << endl;
+	}
+
+	return 0;
 }
