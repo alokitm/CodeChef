d82ee54541702309b5a7f066b0bbe641cf4939d8
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-28 22:47:54 +0530

    Update Bidding.cpp

--- Bidding.cpp
+++ Bidding.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RAINFALL1
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RAINFALL1
 
 #include <bits/stdc++.h>
 using namespace std;
