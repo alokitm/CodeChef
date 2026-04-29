04b3ed4f9d60be19dc8a86895883432ac1745a5f
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-29 23:22:04 +0530

    Update solution in Overspeeding Fine.cpp

--- Overspeeding Fine.cpp
+++ Overspeeding Fine.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RAINFALL1
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RAINFALL1
 
 #include <bits/stdc++.h>
 using namespace std;
