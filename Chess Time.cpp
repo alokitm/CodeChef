d15add81a3f45d20fbea0600c4f310fde75850bb
Alokit Mishra <alokitmishra4@gmail.com>
2026-04-30 22:53:09 +0530

    Refactor Chess Time.cpp for clarity and comments

--- Chess Time.cpp
+++ Chess Time.cpp
@@ -1,17 +1,23 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHESSTIME
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
-	    int n ;
-	    cin >> n ;
-	    
-        cout << (n*60)/20 << endl ;
-	}
-	return 0 ;
 
+	while (t--) {
+	    // n = number of hours
+	    int n;
+	    cin >> n;
+
+	    // Each game takes 20 minutes → total minutes = n * 60
+	    // Number of games = total minutes / 20
+	    cout << (n * 60) / 20 << endl;
+	}
+
+	return 0;
 }
