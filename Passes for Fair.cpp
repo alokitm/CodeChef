d85fef71a71d9d053516e61de5bb23bd6cfda3d9
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-01 22:56:03 +0530

    Improve code readability and add comments
    Refactor code structure for clarity and add comments.

--- Passes for Fair.cpp
+++ Passes for Fair.cpp
@@ -1,24 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FAIRPASS
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
-	    int n,k ;
-	    cin >> n >> k ;
-	    
-	    if(n < k){
-	        cout << "yes" << endl ;
+
+	while (t--) {
+	    // n = number of people, k = pass limit
+	    int n, k;
+	    cin >> n >> k;
+
+	    // Check eligibility for fair pass
+	    if (n < k) {
+	        cout << "yes" << endl;
 	    }
-	    
-	    else{
-	        cout << "no" << endl ;
+	    else {
+	        cout << "no" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
-
