61b8d83b4f1ae098100cc5797192bde7e8d93a94
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-01 22:55:16 +0530

    Create Passes for Fair.cpp

---
+++ Passes for Fair.cpp
@@ -0,0 +1,24 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FAIRPASS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t;
+	while(t--){
+	    int n,k ;
+	    cin >> n >> k ;
+	    
+	    if(n < k){
+	        cout << "yes" << endl ;
+	    }
+	    
+	    else{
+	        cout << "no" << endl ;
+	    }
+	}
+	return 0 ;
+
+}
+
