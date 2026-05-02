dc26c0470490fcad2a800cd31d2ce8da7a6ace08
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-02 22:23:36 +0530

    Update problem link in Read Pages.cpp

--- Read Pages.cpp
+++ Read Pages.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
 
 #include <bits/stdc++.h>
 using namespace std;
