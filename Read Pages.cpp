6f2bd87dc0b2dd7da5ea48e2e5c6a2370f3b647a
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-02 22:23:07 +0530

    Refactor Read Pages.cpp for clarity and structure

--- Read Pages.cpp
+++ Read Pages.cpp
@@ -1,21 +1,29 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
-	    int n,x,y;
+
+	while (t--) {
+	    // n = total pages
+	    // x = pages read per day
+	    // y = number of days
+	    int n, x, y;
 	    cin >> n >> x >> y;
-	    if(n<=x*y){
+
+	    // Check if total readable pages (x * y) cover n
+	    if (n <= x * y) {
 	        cout << "yes" << endl;
 	    }
-	    else{
+	    else {
 	        cout << "no" << endl;
 	    }
 	}
-	return 0;
 
+	return 0;
 }
