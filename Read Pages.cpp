f5619b197ad0179a946a88c2d4bb6e9b065d27bc
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-02 22:24:02 +0530

    Update solution in Read Pages.cpp

--- Read Pages.cpp
+++ Read Pages.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
 
 #include <bits/stdc++.h>
 using namespace std;
