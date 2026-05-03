030a0d7cd65c5cf7fa026a0ad28753a4d7b6bf35
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-03 22:24:20 +0530

    Update Couple Game.cpp

--- Couple Game.cpp
+++ Couple Game.cpp
@@ -1,16 +1,21 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COUGAME
+// Problem: COUGAME (CodeChef)
+// Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COUGAME
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int b, g;
-	    cin >> g >> b;
-	    cout << b-g << endl;
-	}
-	return 0;
+    int t; // number of test cases
+    cin >> t;
 
+    while (t--) {
+        int b, g;
+
+        // Input: number of girls (g) and boys (b)
+        cin >> g >> b;
+
+        // Output: difference between boys and girls
+        cout << (b - g) << endl;
+    }
+
+    return 0;
 }
