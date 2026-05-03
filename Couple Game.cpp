af644d77150b79c97a3c709d380947cf9ed11cb2
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-03 22:12:36 +0530

    Create Couple Game.cpp

---
+++ Couple Game.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COUGAME
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int b, g;
+	    cin >> g >> b;
+	    cout << b-g << endl;
+	}
+	return 0;
+
+}
