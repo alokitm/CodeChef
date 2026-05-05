06c77c552555e3168c2660fb2d437c9933194ac0
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-05 19:22:53 +0530

    Add basic air quality index check

---
+++ Air Quality Index.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    
+    if (x < 100) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
