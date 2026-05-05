34dd9e36d6fd2b669d7d90d5b1dfd7afbc777ce1
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-05 19:24:43 +0530

    Update Air Quality Index.cpp

--- Air Quality Index.cpp
+++ Air Quality Index.cpp
@@ -1,16 +1,21 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    // x = air quality index
     int x;
     cin >> x;
-    
+
+    // Check if air quality is acceptable
     if (x < 100) {
         cout << "YES" << endl;
-    } else {
+    }
+    else {
         cout << "NO" << endl;
     }
-    
+
     return 0;
 }
