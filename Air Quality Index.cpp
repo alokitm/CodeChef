39a810349ef481e5977b1b054c619213eb4ea8e3
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-05 19:25:12 +0530

    Update Air Quality Index.cpp

--- Air Quality Index.cpp
+++ Air Quality Index.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX
 
 #include <bits/stdc++.h>
 using namespace std;
