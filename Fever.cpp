a1db878a1b3805036a9ca39012c3daba3a8d69a1
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-07 18:15:59 +0530

    Implement fever check based on temperature input

---
+++ Fever.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FEVER
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(x>98){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl;
+	    }
+	}
+	return 0 ;
+
+}
