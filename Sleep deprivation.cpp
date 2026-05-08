c17c598ac9d05abe9f9fa4f37a3175b42db2787f
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-08 09:03:05 +0530

    Refactor sleep deprivation logic for clarity

--- Sleep deprivation.cpp
+++ Sleep deprivation.cpp
@@ -1,21 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SLEEP
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = number of sleeping hours
 	    int x;
 	    cin >> x;
-	    if(x<7){
+
+	    // Check if sleep is less than recommended
+	    if (x < 7) {
 	        cout << "yes" << endl;
 	    }
-	    else{
+	    else {
 	        cout << "no" << endl;
 	    }
 	}
-	return 0 ;
 
+	return 0;
 }
