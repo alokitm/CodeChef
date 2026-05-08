749013557b5d0d9054c9f8b04a00be16e725457f
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-08 09:01:44 +0530

    Implement sleep deprivation check logic

---
+++ Sleep deprivation.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SLEEP
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(x<7){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl;
+	    }
+	}
+	return 0 ;
+
+}
