2b3cd564d8e812c586004b2d64b72979f98a4a9f
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-08 09:03:22 +0530

    Update problem link format in Sleep deprivation.cpp

--- Sleep deprivation.cpp
+++ Sleep deprivation.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SLEEP
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SLEEP
 
 #include <bits/stdc++.h>
 using namespace std;
