c98979e62be24538de57a0a6f647b8bde58fe940
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-10 20:16:42 +0530

    Refactor code for clarity and comments

--- Chef and Chapters.cpp
+++ Chef and Chapters.cpp
@@ -1,16 +1,24 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = semesters
+	    // y = courses per semester
+	    // z = credits per course
 	    int x, y, z;
 	    cin >> x >> y >> z;
-	    cout << x*y*z << endl ;
-	}
-	return 0;
 
+	    // Total credits
+	    cout << x * y * z << endl;
+	}
+
+	return 0;
 }
