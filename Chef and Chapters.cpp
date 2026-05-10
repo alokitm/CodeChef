820c96a593177b1f2eca76c137500fca6d1f9371
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-10 20:17:30 +0530

    Update solution in Chef and Chapters.cpp

--- Chef and Chapters.cpp
+++ Chef and Chapters.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
 
 #include <bits/stdc++.h>
 using namespace std;
