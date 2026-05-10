3c5475dea8ea09c42979fedbeca0619324cd5d85
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-10 20:15:57 +0530

    Create Chef and Chapters.cpp

---
+++ Chef and Chapters.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x, y, z;
+	    cin >> x >> y >> z;
+	    cout << x*y*z << endl ;
+	}
+	return 0;
+
+}
