5652c79c2a8c863f6065638a4fe99667344fffc4
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-11 17:41:22 +0530

    Create Water Requirement.cpp

---
+++ Water Requirement.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    cout << n*2 << endl ;
+	}
+	return 0;
+
+}
