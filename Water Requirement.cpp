35663469346fca46700377d90666d64534b60851
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-11 17:43:35 +0530

    Update Water Requirement.cpp

--- Water Requirement.cpp
+++ Water Requirement.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
 
 #include <bits/stdc++.h>
 using namespace std;
