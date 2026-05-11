030f5dece65b2fdd2c99216d80b0186a407349c2
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-11 17:42:50 +0530

    Refactor water requirement calculation logic

--- Water Requirement.cpp
+++ Water Requirement.cpp
@@ -1,16 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // n = number of liters required
 	    int n;
 	    cin >> n;
-	    cout << n*2 << endl ;
-	}
-	return 0;
 
+	    // Total water required
+	    cout << n * 2 << endl;
+	}
+
+	return 0;
 }
