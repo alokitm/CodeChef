1546f4591ebe0c5f98a9ee4ee5577386d43cf5af
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-12 22:19:16 +0530

    Create Lunchtime.cpp

---
+++ Lunchtime.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(x>=1 && x<=4){
+	         cout << "yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	   
+	}
+	return 0;
+
+}
