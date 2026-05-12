c32660be07e5ef631be89f94ba1a19d13dfa093b
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-12 22:20:21 +0530

    Refactor Lunchtime.cpp for clarity and structure

--- Lunchtime.cpp
+++ Lunchtime.cpp
@@ -1,22 +1,27 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = contest rank
 	    int x;
 	    cin >> x;
-	    if(x>=1 && x<=4){
-	         cout << "yes" << endl ;
-	    }
-	    else{
-	        cout << "No" << endl ;
-	    }
-	   
-	}
-	return 0;
 
+	    // Check if rank is between 1 and 4 (inclusive)
+	    if (x >= 1 && x <= 4) {
+	        cout << "yes" << endl;
+	    }
+	    else {
+	        cout << "No" << endl;
+	    }
+	}
+
+	return 0;
 }
