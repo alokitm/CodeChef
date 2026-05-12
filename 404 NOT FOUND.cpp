ccbe7ae1dee14eb819aa2b1a678b9f0978b6df32
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-12 23:05:46 +0530

    Update 404 NOT FOUND.cpp

--- 404 NOT FOUND.cpp
+++ 404 NOT FOUND.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
 
 #include <bits/stdc++.h>
 using namespace std;
