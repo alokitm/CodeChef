14a2d67617e766851e4afd1f65791ab5047e1463
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-13 21:02:25 +0530

    Update problem link in Good Investment or Not.cpp

--- Good Investment or Not.cpp
+++ Good Investment or Not.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
 
 #include <bits/stdc++.h>
 using namespace std;
