f8fde8afe7c79d755a64afb57930ef3d3d42d6be
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-13 20:43:47 +0530

    Create Good Investment or Not.cpp

---
+++ Good Investment or Not.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INVESTMENT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x,y;
+	    cin >> x >> y;
+	    if(x>=2*y){
+	         cout << "yes" << endl ;
+	    }
+	    else{
+	        cout << "No" << endl ;
+	    }
+	   
+	}
+	return 0;
+
+}
