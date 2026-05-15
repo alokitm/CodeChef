ce65c683f36cd4f1f33c5e8907990db7504e7dfd
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-15 20:55:09 +0530

    Update Final Population.cpp with corrections
    Fixed syntax errors and added necessary includes.

---
+++ Final Population.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x, y, z;
+	    cin >> x >> y >> z;
+	         cout << x-y+z << endl ;
+	   
+	}
+	return 0;
+
+}
