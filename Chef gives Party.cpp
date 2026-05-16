4a933d0c85cefcbafbee842db18ab6c8d89b456e
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-16 13:16:56 +0530

    Refactor code for clarity and consistency

--- Chef gives Party.cpp
+++ Chef gives Party.cpp
@@ -1,21 +1,29 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARTY2
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // n = number of friends
+	    // x = cost per person
+	    // k = total budget
 	    int n, x, k;
 	    cin >> n >> x >> k;
-	    if(k>=n*x){
+
+	    // Check if budget is sufficient
+	    if (k >= n * x) {
 	        cout << "yes" << endl;
 	    }
-	    else{
+	    else {
 	        cout << "no" << endl;
 	    }
 	}
-	return 0;
 
+	return 0;
 }
