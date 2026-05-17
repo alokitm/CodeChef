9ed1457b1ad61126248012039627adefe8829e09
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-17 14:54:44 +0530

    Update Time Complexity.cpp

--- Time Complexity.cpp
+++ Time Complexity.cpp
@@ -1,21 +1,28 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/COMPLEXITY
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
-	    int x,y;
+
+	while (t--) {
+	    // x = old complexity
+	    // y = new complexity
+	    int x, y;
 	    cin >> x >> y;
-	    if(x>y){
+
+	    // Check if old complexity is greater
+	    if (x > y) {
 	        cout << "yes" << endl;
 	    }
-	    else{
+	    else {
 	        cout << "no" << endl;
 	    }
 	}
-	return 0;
 
+	return 0;
 }
