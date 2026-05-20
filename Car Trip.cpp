cfdcc22b5a26196f1c6fc77da3d61c38f9aee053
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-20 21:36:42 +0530

    Update problem link in Car Trip.cpp

--- Car Trip.cpp
+++ Car Trip.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CARTRIP
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CARTRIP
 
 #include <bits/stdc++.h>
 using namespace std;
