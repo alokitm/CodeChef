b4cfe10b43cb10fe52f3ff8978588ed1a9f76ab8
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-20 21:27:08 +0530

    Refactor Biryani class code for clarity and comments

--- Biryani classes.cpp
+++ Biryani classes.cpp
@@ -1,19 +1,24 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// T = number of test cases
 	int T;
 	cin >> T;
-	while(T--){
-	    int X, Y ;
-	    cin >> X ;
-	    cin >> Y ;
-	    cout << X*Y << endl;
+
+	while (T--) {
+	    // X = number of plates
+	    // Y = cost per plate
+	    int X, Y;
+	    cin >> X;
+	    cin >> Y;
+
+	    // Total cost
+	    cout << X * Y << endl;
 	}
-	return 0 ;
 
+	return 0;
 }
-
-
