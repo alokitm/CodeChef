392ff6f09749b8032853712ecd6949ca235065ad
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-20 21:27:24 +0530

    Update problem link in Biryani classes.cpp

--- Biryani classes.cpp
+++ Biryani classes.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
 
 #include <bits/stdc++.h>
 using namespace std;
