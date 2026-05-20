85ec6f2915906a8ff486bd57031b7a88da38aac1
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-20 21:21:31 +0530

    Refactor input/output handling in main function

--- Interior Design.cpp
+++ Interior Design.cpp
@@ -1,16 +1,23 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INTRDSGN
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
-	    int x1, y1, x2, y2;
-	    cin >> x1 >> y1 >> x2 >> y2 ;
-	    cout << min(x1+y1, x2+y2) << endl ;
-	}
-	return 0;
 
+	while (t--) {
+	    // x1, y1 = dimensions/design values of first option
+	    // x2, y2 = dimensions/design values of second option
+	    int x1, y1, x2, y2;
+	    cin >> x1 >> y1 >> x2 >> y2;
+
+	    // Output the minimum total value
+	    cout << min(x1 + y1, x2 + y2) << endl;
+	}
+
+	return 0;
 }
