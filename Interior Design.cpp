9b71da59b4efe81e568471ffe557d9b120383570
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-20 21:21:55 +0530

    Update problem link in Interior Design.cpp

--- Interior Design.cpp
+++ Interior Design.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INTRDSGN
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INTRDSGN
 
 #include <bits/stdc++.h>
 using namespace std;
