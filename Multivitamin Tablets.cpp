30ca703149642f67ea28c876967006cce25edeb0
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-21 18:35:19 +0530

    Implement main logic for multivitamin tablet problem

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TABLETS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin >> t;
+	while(t--){
+	    int x, y ;
+	    cin >> x >> y;
+	    if(y>=3*x){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout  << "no" << endl ;
+	    }
+	}
+return 0 ;
+}
