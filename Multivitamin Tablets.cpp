55f0ab57d59557df6a9aef2df8a9373bed4700be
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-21 18:37:39 +0530

    Refactor tablet availability check logic

--- Multivitamin Tablets.cpp
+++ Multivitamin Tablets.cpp
@@ -1,20 +1,28 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TABLETS
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
-	    int x, y ;
+
+	while (t--) {
+	    // x = required tablets
+	    // y = available tablets
+	    int x, y;
 	    cin >> x >> y;
-	    if(y>=3*x){
+
+	    // Check if available tablets are at least 3 times required
+	    if (y >= 3 * x) {
 	        cout << "yes" << endl;
 	    }
-	    else{
-	        cout  << "no" << endl ;
+	    else {
+	        cout << "no" << endl;
 	    }
 	}
-return 0 ;
+
+	return 0;
 }
