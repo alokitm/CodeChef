7898cb9a3aa021b7401719fd88b85e7dc634f02d
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-22 22:45:57 +0530

    Update Six Friends.cpp

--- Six Friends.cpp
+++ Six Friends.cpp
@@ -1,16 +1,23 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SIXFRIENDS
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = cost option 1
+	    // y = cost option 2
 	    int x, y;
 	    cin >> x >> y;
-	    cout << min(3*x, 2*y) << endl;
-	}
-	return 0 ;
 
+	    // Choose the minimum cost option
+	    cout << min(3 * x, 2 * y) << endl;
+	}
+
+	return 0;
 }
