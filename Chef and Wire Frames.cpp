7332c91478ff3d21e4623125b3d99f330a32d702
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-23 20:14:06 +0530

    Update Chef and Wire Frames.cpp

--- Chef and Wire Frames.cpp
+++ Chef and Wire Frames.cpp
@@ -1,15 +1,24 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWIREFRAME
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t; 
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // n = length
+	    // m = breadth
+	    // x = cost per unit
 	    int n, m, x;
 	    cin >> n >> m >> x;
-	    cout << 2*x*(n+m) << endl;
+
+	    // Total wireframe cost
+	    cout << 2 * x * (n + m) << endl;
 	}
-return 0 ;
+
+	return 0;
 }
