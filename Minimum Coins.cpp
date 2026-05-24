cd475ed4682f63384882687d822cdc3ed3b316f1
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-24 23:45:36 +0530

    Update Minimum Coins.cpp

--- Minimum Coins.cpp
+++ Minimum Coins.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
 
 #include <bits/stdc++.h>
 using namespace std;
