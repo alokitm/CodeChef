b15f23369c6bb9f98d1e961b7cf83eb82c1bece7
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-24 23:45:08 +0530

    Update Minimum Coins.cpp

--- Minimum Coins.cpp
+++ Minimum Coins.cpp
@@ -1,15 +1,22 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
+	// t = number of test cases
+	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = given amount
 	    int x;
 	    cin >> x;
-	    cout << x%10 << endl;
+
+	    // Minimum coins required (remainder after division by 10)
+	    cout << x % 10 << endl;
 	}
-return 0;
+
+	return 0;
 }
