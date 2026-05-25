2ba09226dee732c05f55131832b0c19a81338d4e
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-25 22:30:42 +0530

    Update problem link comment in Spice Level.cpp

--- Spice Level.cpp
+++ Spice Level.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENSPICE
+//Codechef  Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENSPICE
 
 #include <bits/stdc++.h>
 using namespace std;
