678af15899aa5813829ad5cbfd25a76e75fba71c
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-25 22:29:28 +0530

    Update Spice Level.cpp

--- Spice Level.cpp
+++ Spice Level.cpp
@@ -1,24 +1,30 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENSPICE
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = spice level
 	    int x;
 	    cin >> x;
-	    if(x<4){
+
+	    // Categorize spice level
+	    if (x < 4) {
 	        cout << "mild" << endl;
 	    }
-	    else if(x>=4 && x<7){
-            cout << "medium" << endl;
+	    else if (x >= 4 && x < 7) {
+	        cout << "medium" << endl;
 	    }
-	    else{
+	    else {
 	        cout << "hot" << endl;
 	    }
 	}
-	return 0;
 
+	return 0;
 }
