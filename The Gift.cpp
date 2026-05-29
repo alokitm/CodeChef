cc287b1268468833046d165e67cbe484aafeb02b
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-29 22:45:16 +0530

    Update problem link comment in The Gift.cpp

--- The Gift.cpp
+++ The Gift.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
 
 #include <bits/stdc++.h>
 using namespace std;
