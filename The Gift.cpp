0f3d5daf1ed1ff48520d16c323801af95d578081
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-29 22:43:23 +0530

    Create The Gift.cpp

---
+++ The Gift.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+	    int x, n, m;
+	    cin >> x >> n >> m;
+	    int budget = x+m;
+	    if(n>budget){
+	        cout << "no" << endl ;
+	    }
+	    else{
+	        cout << "yes" << endl ;
+	    }
+
+return 0 ;
+}
