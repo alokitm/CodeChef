f9453d1d3325afb5cdf16a5c12fbe2be969b2023
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-30 21:28:05 +0530

    Refactor Reach Home.cpp for clarity and structure

--- Reach Home.cpp
+++ Reach Home.cpp
@@ -1,21 +1,28 @@
+// Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACH_HOME
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	// t = number of test cases
 	int t;
 	cin >> t;
-	while(t--){
+
+	while (t--) {
+	    // x = speed per unit time
+	    // y = required distance
 	    int x, y;
 	    cin >> x >> y;
-	    if(5*x>=y){
-	        cout << "yes" << endl ;
+
+	    // Check if Chef can reach home within 5 units of time
+	    if (5 * x >= y) {
+	        cout << "yes" << endl;
 	    }
-	    else{
-	        cout << "no" << endl ;
+	    else {
+	        cout << "no" << endl;
 	    }
 	}
-	return 0;
 
+	return 0;
 }
