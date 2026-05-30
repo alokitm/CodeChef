b61e6a18541ed9b565180011625cb4b9aa4066ee
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-30 21:28:35 +0530

    Update comment with correct Codechef problem link

--- Reach Home.cpp
+++ Reach Home.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACH_HOME
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACH_HOME
 
 #include <bits/stdc++.h>
 using namespace std;
