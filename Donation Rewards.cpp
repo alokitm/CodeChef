9b780c2b5be82107188cc9f54679615284e4f2ef
Alokit Mishra <alokitmishra4@gmail.com>
2026-05-31 21:04:38 +0530

    Create Donation Rewards.cpp

---
+++ Donation Rewards.cpp
@@ -0,0 +1,24 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOREWARD
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int  t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x ;
+	    if(x<=3){
+	        cout << "bronze" << endl ;
+	    }
+	    else if(x>3 && x<=6){
+	        cout << "silver" << endl ;
+	    }
+	    else{
+	        cout << "gold" << endl ;
+	    }
+	}
+return 0;
+}
+
