8003ea889cd91d36920c57915c832c366efca12c
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-01 22:50:03 +0530

    Create True and False Paper.cpp

---
+++ True and False Paper.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TFPAPER
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,k;
+	    cin >> n >> k;
+	    cout << n-k << endl;
+	}
+
+}
