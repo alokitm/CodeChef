e5098d23e2114631cccdde25dc1d4da3cdde7c48
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-01 22:55:31 +0530

    Update comment with correct link format

--- The Cheaper Cab.cpp
+++ The Cheaper Cab.cpp
@@ -1,4 +1,4 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
+// Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
 #include <bits/stdc++.h>
 using namespace std;
 
