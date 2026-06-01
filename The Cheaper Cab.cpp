57e2ab7ffdee09921c1f84db9b8d84daca0f1b86
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-01 23:59:06 +0530

    Update The Cheaper Cab.cpp

--- The Cheaper Cab.cpp
+++ The Cheaper Cab.cpp
@@ -1,4 +1,4 @@
-// Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
+// Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
 #include <bits/stdc++.h>
 using namespace std;
 
