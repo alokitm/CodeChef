62cf4620f7f39e3b9750ad48e7ea9851d8b7b262
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-02 23:32:30 +0530

    Update Discount.cpp

--- Discount.cpp
+++ Discount.cpp
@@ -1,16 +1,28 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT
+
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int x;
-	    cin >> x;
-	    cout << 100-x << endl ;
-	}
-	return 0;
 
+    // Variable to store number of test cases
+    int t;
+    cin >> t;
+
+    // Loop through all test cases
+    while(t--) {
+
+        // Discount percentage
+        int x;
+        cin >> x;
+
+        // Original price = 100
+        // Final price after discount = 100 - x
+        cout << 100 - x << endl;
+    }
+
+    return 0;
 }
