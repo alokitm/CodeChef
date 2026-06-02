896722026de00de1cdc8d8ea97d501e020010734
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-02 23:31:49 +0530

    Create Discount.cpp

---
+++ Discount.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    cout << 100-x << endl ;
+	}
+	return 0;
+
+}
