68bf1acca5a1ea1c6fd45e8d6f1bdf0155befe52
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-03 21:46:22 +0530

    Update Height of Rationals.cpp

--- Height of Rationals.cpp
+++ Height of Rationals.cpp
@@ -1,4 +1,4 @@
-// Problem Link: Not Provided
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
 
 // Time Complexity: O(1)
 // Space Complexity: O(1)
