a364f9c3e4a969ad863617e16752428e6993b4ac
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-03 21:45:07 +0530

    Create Height of Rationals.cpp

---
+++ Height of Rationals.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	    int a,b;
+	    cin >> a >> b;
+	    cout << max(a,b) << endl ;
+
+	return 0;
+
+}
