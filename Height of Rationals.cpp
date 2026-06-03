aee4de817607a03c7a89120a5e84ff9ef3523f8c
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-03 21:45:48 +0530

    Update Height of Rationals.cpp

--- Height of Rationals.cpp
+++ Height of Rationals.cpp
@@ -1,14 +1,19 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
+// Problem Link: Not Provided
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	
-	    int a,b;
-	    cin >> a >> b;
-	    cout << max(a,b) << endl ;
 
-	return 0;
+    // Variables to store two numbers
+    int a, b;
+    cin >> a >> b;
 
+    // Print the maximum of the two numbers
+    cout << max(a, b) << endl;
+
+    return 0;
 }
