4e0f46fd815c53d0e8015178e5405321ff45250a
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-04 22:22:31 +0530

    Update Instagram.cpp

--- Instagram.cpp
+++ Instagram.cpp
@@ -1,21 +1,33 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INSTAGRAM
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INSTAGRAM
+
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int x, y;
-	    cin >> x >> y ;
-	    if(x>10*y){
-	        cout << "yes" << endl;
-	    }
-	    else{
-	        cout << "no" << endl;
-	    }
-	}
-	return 0;
 
+    // Variable to store number of test cases
+    int t;
+    cin >> t;
+
+    // Process each test case
+    while(t--) {
+
+        // x = number of followers
+        // y = number of following
+        int x, y;
+        cin >> x >> y;
+
+        // Check if followers are greater than 10 times following
+        if(x > 10 * y) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
 }
