ffa13c5163423bf117214465cd37f70a067ce961
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-05 21:21:38 +0530

    Update Volume Control.cpp

--- Volume Control.cpp
+++ Volume Control.cpp
@@ -1,16 +1,29 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/VOLCONTROL
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/VOLCONTROL
+
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int x, y;
-	    cin >> x >> y ;
-	    cout << abs(x-y) << endl ;
-	}
-	return 0;
 
+    // Variable to store number of test cases
+    int t;
+    cin >> t;
+
+    // Process each test case
+    while(t--) {
+
+        // x = current volume
+        // y = desired volume
+        int x, y;
+        cin >> x >> y;
+
+        // Minimum button presses required
+        // is equal to absolute difference
+        cout << abs(x - y) << endl;
+    }
+
+    return 0;
 }
