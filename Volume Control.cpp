1a59e01d93b91306002b3cbbab83f23fda37292b
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-05 21:22:54 +0530

    Update comment with Codechef problem link

--- Volume Control.cpp
+++ Volume Control.cpp
@@ -1,4 +1,5 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/VOLCONTROL
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/VOLCONTROL
+
 
 // Time Complexity: O(t)
 // Space Complexity: O(1)
