6890a0ab7d2d22cf125268eb49512b3e9510509e
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-07 22:30:58 +0530

    Update is it hot or cold.cpp

--- is it hot or cold.cpp
+++ is it hot or cold.cpp
@@ -1,22 +1,35 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
+
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int c;
-	    cin >> c;
-	    if(c>20){
-	        cout << "hot" << endl;
-	    }
-	    else{
-	        cout << "cold" << endl ;
-	    }
 
-	}
-	return 0;
+    // Variable to store number of test cases
+    int t;
+    cin >> t;
 
+    // Process each test case
+    while(t--) {
+
+        // c = temperature in degree Celsius
+        int c;
+        cin >> c;
+
+        // If temperature is greater than 20
+        // then weather is considered hot
+        if(c > 20) {
+            cout << "hot" << endl;
+        }
+        else {
+
+            // Otherwise weather is cold
+            cout << "cold" << endl;
+        }
+    }
+
+    return 0;
 }
