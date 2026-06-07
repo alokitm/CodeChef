1d500d2b82da0a7bb39c9abd33c6c098b978b966
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-07 22:31:21 +0530

    Update is it hot or cold.cpp

--- is it hot or cold.cpp
+++ is it hot or cold.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
 
 // Time Complexity: O(t)
 // Space Complexity: O(1)
