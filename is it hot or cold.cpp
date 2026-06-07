276fad6fddb9a9bb2b2aec9bdc668700ab9c732c
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-07 22:29:50 +0530

    Create is it hot or cold.cpp

---
+++ is it hot or cold.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int c;
+	    cin >> c;
+	    if(c>20){
+	        cout << "hot" << endl;
+	    }
+	    else{
+	        cout << "cold" << endl ;
+	    }
+
+	}
+	return 0;
+
+}
