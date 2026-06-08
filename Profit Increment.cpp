511e03d1c5a1c3f9b72162bcff5e1b4052ec0e2a
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-08 22:57:07 +0530

    Create Profit Increment.cpp

---
+++ Profit Increment.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PROINC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+    
+	    int x, y;
+	    cin >> x >> y;
+	    cout << x*0.1 + y << endl ;
+	}
+	return 0;
+
+}
