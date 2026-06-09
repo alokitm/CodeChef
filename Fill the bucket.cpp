06ef4e46c6cb6771a75d00691ac50a429000b38b
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-09 21:47:52 +0530

    Create Fill the bucket.cpp

---
+++ Fill the bucket.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FBC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int k,x;
+	    cin >> k >> x;
+	   cout << k-x << endl;
+	}
+	return 0;
+
+}
