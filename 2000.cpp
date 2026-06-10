11f7114657a082aa39ca8a24fe9aaa0f185ec185
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-10 23:52:22 +0530

    Update 2000.cpp

--- 2000.cpp
+++ 2000.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
 
 #include <bits/stdc++.h>
 using namespace std;
