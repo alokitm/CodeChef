2faf27435aee8dabe5c43b668dc540835258ebe8
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-10 23:53:10 +0530

    Update 2000.cpp

--- 2000.cpp
+++ 2000.cpp
@@ -1,15 +1,20 @@
 // Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000
 
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// N = number of years
-	int N;
-	cin >> N;
 
-	// Each year gives 4 units
-	cout << N * 4;
+    // N = number of years
+    int N;
+    cin >> N;
 
-	return 0;
+    // Each year contributes 4 units
+    // Total = N * 4
+    cout << N * 4;
+
+    return 0;
 }
