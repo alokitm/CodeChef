6b4a0879a41f3fec27c53b1b030af0dd6fcbdb74
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-13 20:50:38 +0530

    Update Parliament.cpp

--- Parliament.cpp
+++ Parliament.cpp
@@ -1,22 +1,33 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARLIAMENT
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARLIAMENT
+
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int n,x;
-	    cin >> n >> x;
-	    if(2*x>=n){
-	        cout << "yes" << endl;
-	    }
-	    else{
-	        cout << "no" << endl ;
-	    }
 
-	}
-	return 0;
+    // Variable to store number of test cases
+    int t;
+    cin >> t;
 
+    // Process each test case
+    while(t--) {
+
+        // n = total seats in parliament
+        // x = seats won by the party
+        int n, x;
+        cin >> n >> x;
+
+        // A party forms majority if it has at least half the seats
+        if(2 * x >= n) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
 }
