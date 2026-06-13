351ffd486023a04aded1c35978c8a228a68a5a52
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-13 20:49:41 +0530

    Create Parliament.cpp

---
+++ Parliament.cpp
@@ -0,0 +1,22 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARLIAMENT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,x;
+	    cin >> n >> x;
+	    if(2*x>=n){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl ;
+	    }
+
+	}
+	return 0;
+
+}
