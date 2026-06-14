9cbd4b1372d22aec32e78ed57553df27ac13d0a3
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-14 18:42:45 +0530

    Create Find Remainder.cpp

---
+++ Find Remainder.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int a,b;
+	    cin >> a >> b;
+	    cout << a%b << endl;
+
+	}
+	return 0;
+
+}
