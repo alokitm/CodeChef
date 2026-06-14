4bf1c940409c7c23883b98f34a62704d5578fab8
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-14 18:43:44 +0530

    Improve code readability and add comments in Find Remainder
    Refactor code structure for clarity and add comments.

--- Find Remainder.cpp
+++ Find Remainder.cpp
@@ -1,17 +1,28 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FLOW002
+
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--){
-	    int a,b;
-	    cin >> a >> b;
-	    cout << a%b << endl;
 
-	}
-	return 0;
+    // Variable to store number of test cases
+    int t;
+    cin >> t;
 
+    // Process each test case
+    while(t--) {
+
+        // a = dividend
+        // b = divisor
+        int a, b;
+        cin >> a >> b;
+
+        // Print remainder when a is divided by b
+        cout << a % b << endl;
+    }
+
+    return 0;
 }
