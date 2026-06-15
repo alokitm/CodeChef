581a82f33adb6c272a3e2d55953c33ebd5d85b56
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-15 17:19:10 +0530

    Update 404 NOT FOUND.cpp

--- 404 NOT FOUND.cpp
+++ 404 NOT FOUND.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
 
 // Time Complexity: O(1) & Space Complexity: O(1)
 
