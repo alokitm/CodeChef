d4cb656fbc8f4db9c968f974ef69b6fe8734d0af
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-15 17:18:35 +0530

    Update 404 NOT FOUND.cpp

--- 404 NOT FOUND.cpp
+++ 404 NOT FOUND.cpp
@@ -1,20 +1,31 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+
     // X = status code
     int X;
 
-    // Input
+    // Input status code
     cin >> X;
 
-    // Check if status code is 404 in the program
-    if (X == 404)
+    // Check whether the status code is 404
+    if (X == 404) {
+
+        // If status code is 404
+        // then page is not found
         cout << "NOT FOUND";
-    else
+    }
+    else {
+
+        // Otherwise page is found
         cout << "FOUND";
+    }
 
     return 0;
 }
