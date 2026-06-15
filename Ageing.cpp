28525364e781274e7a1757263ef0967b07cbc02c
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-15 17:20:07 +0530

    Update Ageing.cpp

--- Ageing.cpp
+++ Ageing.cpp
@@ -1,26 +1,37 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = speed of Tom & Y = speed of Jerry
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if Jerry is faster than Tom
-	    if (Y > X) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = speed of Tom
+        // Y = speed of Jerry
+        int X, Y;
+        cin >> X >> Y;
+
+        // Check if Jerry is faster than Tom
+        if (Y > X) {
+
+            // Jerry can escape
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Tom can catch Jerry
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
