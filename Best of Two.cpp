5b3524851699aed2b86521a32d29070c48db1b83
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-16 22:42:45 +0530

    Update Best of Two.cpp

--- Best of Two.cpp
+++ Best of Two.cpp
@@ -1,5 +1,4 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
 
 #include <bits/stdc++.h>
 using namespace std;
