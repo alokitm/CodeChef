da6d41cb909f15aaf65faa53d5516bc64316aaf5
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-16 22:41:46 +0530

    Update Battery Health.cpp

--- Battery Health.cpp
+++ Battery Health.cpp
@@ -1,22 +1,28 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // M = number of rows
-	    // N = number of columns
-	    int M, N;
-	    cin >> M >> N;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Total number of words
-	    cout << M * N << endl;
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // M = number of rows
+        // N = number of columns
+        int M, N;
+        cin >> M >> N;
+
+        // Total words = rows × columns
+        cout << M * N << endl;
+    }
+
+    return 0;
 }
