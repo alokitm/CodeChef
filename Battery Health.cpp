737c882aa66dbbe4cc82866f0d8790866293ae07
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-16 22:42:07 +0530

    Update Battery Health.cpp

--- Battery Health.cpp
+++ Battery Health.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
