30cb1bde2a892769c8c1ba94256a2deca1491036
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-18 21:24:36 +0530

    Update Best of Two.cpp

--- Best of Two.cpp
+++ Best of Two.cpp
@@ -1,21 +1,27 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X and Y = scores
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Output the maximum of the two
-	    cout << max(X, Y) << endl;
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X and Y = scores obtained in two attempts
+        int X, Y;
+        cin >> X >> Y;
+
+        // Print the higher score among the two attempts
+        cout << max(X, Y) << endl;
+    }
+
+    return 0;
 }
