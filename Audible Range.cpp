88dca368dd0bdcdad6dcd7d52229537fc5621ce6
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-18 21:24:06 +0530

    Update Audible Range.cpp

--- Audible Range.cpp
+++ Audible Range.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
 
 // Time Complexity: O(T) & Space Complexity: O(1)
 
