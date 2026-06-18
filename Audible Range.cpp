4ede45e67c4102fc14b06c116193edf79be46115
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-18 21:23:47 +0530

    Update Audible Range.cpp

--- Audible Range.cpp
+++ Audible Range.cpp
@@ -1,7 +1,6 @@
 // Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
 
-// Time Complexity: O(T)
-// Space Complexity: O(1)
+// Time Complexity: O(T) & Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
