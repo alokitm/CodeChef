c16343c7ea8be010b01076e16b289340591c6e06
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-18 21:23:32 +0530

    Update Audible Range.cpp

--- Audible Range.cpp
+++ Audible Range.cpp
@@ -1,23 +1,37 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AUDIBLE
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int T ; 
-	cin  >> T ;
-	while(T--){
-	    int X; 
-	    cin >> X;
-	    if(X >= 67 && X <= 45000){
-	        cout << "Yes" << endl;
-	    }
-	    else{
-	        cout << "No" << endl;
-	    }
-	        
-	}
-	return 0 ;
 
+    // T = number of test cases
+    int T;
+    cin >> T;
+
+    // Process each test case
+    while (T--) {
+
+        // X = frequency in Hertz (Hz)
+        int X;
+        cin >> X;
+
+        // Check whether the frequency lies in the audible range
+        // Audible range: 67 Hz to 45000 Hz (inclusive)
+        if (X >= 67 && X <= 45000) {
+
+            // Frequency is audible
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Frequency is not audible
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
