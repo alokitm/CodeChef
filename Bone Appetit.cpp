91b8946f7cd060690e4bd20b8307ec6179dc7471
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-20 22:41:29 +0530

    Update Bone Appetit.cpp

--- Bone Appetit.cpp
+++ Bone Appetit.cpp
@@ -1,8 +1,6 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
 
-// Time Complexity: O(1)
-// Space Complexity: O(1)
+// Time Complexity: O(1) & Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
