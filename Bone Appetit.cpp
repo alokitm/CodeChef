5e956d677e5c3515b4d7d0590d6bb828e089e2e2
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-20 22:41:04 +0530

    Update Bone Appetit.cpp

--- Bone Appetit.cpp
+++ Bone Appetit.cpp
@@ -1,24 +1,29 @@
 // Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
 
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// N = number of items of type 1
-	// M = number of items of type 2
-	int N, M;
 
-	// X = cost of type 1 item
-	// Y = cost of type 2 item
-	int X, Y;
+    // N = number of items of type 1
+    // M = number of items of type 2
+    int N, M;
 
-	// Input
-	cin >> N >> M;
-	cin >> X >> Y;
+    // X = cost of one type 1 item
+    // Y = cost of one type 2 item
+    int X, Y;
 
-	// Total cost calculation
-	cout << N * X + M * Y;
+    // Input values
+    cin >> N >> M;
+    cin >> X >> Y;
 
-	return 0;
+    // Calculate total cost:
+    // (N × X) + (M × Y)
+    cout << N * X + M * Y;
+
+    return 0;
 }
