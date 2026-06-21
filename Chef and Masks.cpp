00a9798949421c1c0c4187b67ad6c50c7bfcea3f
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-21 21:57:44 +0530

    Update Chef and Masks.cpp

--- Chef and Masks.cpp
+++ Chef and Masks.cpp
@@ -1,22 +1,38 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CMASKS
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CMASKS
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t ;
-	cin >> t;
-	while(t--){
-	    int x, y;
-	    cin >> x >> y;
-	    
-	    if(100*x<10*y){
-	        cout << "disposable" << endl;
-	    }
-	    else{
-	        cout << "cloth" << endl;
-	    }
-	}
 
+    // T = number of test cases
+    int T;
+    cin >> T;
+
+    // Process each test case
+    while (T--) {
+
+        // X = cost of one disposable mask
+        // Y = cost of one cloth mask
+        int X, Y;
+        cin >> X >> Y;
+
+        // Cost of using disposable masks for 100 days = 100 * X
+        // Cost of using cloth masks for 100 days = 10 * Y
+        if (100 * X < 10 * Y) {
+
+            // Disposable masks are cheaper
+            cout << "disposable" << endl;
+        }
+        else {
+
+            // Cloth masks are cheaper or equal in cost
+            cout << "cloth" << endl;
+        }
+    }
+
+    return 0;
 }
