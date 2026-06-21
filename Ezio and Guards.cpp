f104baa9739dd7c95eac8e7f8ff2dc5c6479f90e
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-21 21:52:53 +0530

    Add URL comment for problem reference

---
+++ Ezio and Guards.cpp
@@ -0,0 +1,23 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANIPULATE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+	    int x, y;
+	    cin >> x >> y;
+	    if(y<=x){
+	        cout << "yes" << endl;
+	    }
+	    else{
+	        cout << "no" << endl ;
+	    }
+
+	}
+	return 0;
+
+}
