9d6d407ad65835147a35bc2599723156e7290bea
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-22 21:47:21 +0530

    Update Bull or Bear.cpp

--- Bull or Bear.cpp
+++ Bull or Bear.cpp
@@ -1,28 +1,45 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BULLBEAR
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BULLBEAR
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = cost price, Y = selling price
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Determine profit, loss, or neutral
-	    if (Y > X) {
-	        cout << "Profit" << endl;
-	    }
-	    else if (Y == X) {
-	        cout << "Neutral" << endl;
-	    }
-	    else {
-	        cout << "Loss" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = cost price
+        // Y = selling price
+        int X, Y;
+        cin >> X >> Y;
+
+        // Compare selling price with cost price
+        if (Y > X) {
+
+            // Selling price is greater than cost price
+            // Therefore, there is a profit
+            cout << "Profit" << endl;
+        }
+        else if (Y == X) {
+
+            // Selling price equals cost price
+            // Therefore, there is no profit and no loss
+            cout << "Neutral" << endl;
+        }
+        else {
+
+            // Selling price is less than cost price
+            // Therefore, there is a loss
+            cout << "Loss" << endl;
+        }
+    }
+
+    return 0;
 }
