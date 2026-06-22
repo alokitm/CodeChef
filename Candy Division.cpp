2160a192048a5882a2c77b6148457e2cbce5aa15
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-22 21:48:25 +0530

    Update Candy Division.cpp

--- Candy Division.cpp
+++ Candy Division.cpp
@@ -1,24 +1,36 @@
+// Problem Link: Not Provided
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // N = given number
-	    int N;
-	    cin >> N;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if N is divisible by 3
-	    if (N % 3 == 0) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = given number
+        int N;
+        cin >> N;
+
+        // Check whether N is divisible by 3
+        if (N % 3 == 0) {
+
+            // Number is divisible by 3
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Number is not divisible by 3
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
