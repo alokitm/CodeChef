017cfbec2489c5f3b597160567148c1d61ff31eb
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-23 22:44:28 +0530

    Update Car Trip.cpp

--- Car Trip.cpp
+++ Car Trip.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CARTRIP
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CARTRIP
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
