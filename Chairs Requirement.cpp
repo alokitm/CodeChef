79e6a818e9ced7c32603f6d07506bda90b28018b
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-23 22:46:11 +0530

    Update Chairs Requirement.cpp

--- Chairs Requirement.cpp
+++ Chairs Requirement.cpp
@@ -1,26 +1,37 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHAIRS_
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHAIRS_
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = total chairs needed, Y = chairs already available
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Remaining chairs required (cannot be negative)
-	    if (X - Y > 0) {
-	        cout << X - Y << endl;
-	    }
-	    else {
-	        cout << "0" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = total chairs required
+        // Y = chairs already available
+        int X, Y;
+        cin >> X >> Y;
+
+        // Calculate additional chairs needed
+        if (X - Y > 0) {
+
+            // More chairs are required
+            cout << X - Y << endl;
+        }
+        else {
+
+            // Available chairs are sufficient
+            cout << 0 << endl;
+        }
+    }
+
+    return 0;
 }
