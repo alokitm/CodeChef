5aad652c2d6ae8faf53b9c1fa1f8d751d4f262ce
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-24 21:36:14 +0530

    Update Chef and Chapters.cpp

--- Chef and Chapters.cpp
+++ Chef and Chapters.cpp
@@ -1,23 +1,30 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SEMCOURSES
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// t = number of test cases
-	int t;
-	cin >> t;
 
-	while (t--) {
-	    // x = semesters
-	    // y = courses per semester
-	    // z = credits per course
-	    int x, y, z;
-	    cin >> x >> y >> z;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Total credits
-	    cout << x * y * z << endl;
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = number of semesters
+        // Y = number of courses per semester
+        // Z = credits per course
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        // Total credits earned
+        // = semesters × courses per semester × credits per course
+        cout << X * Y * Z << endl;
+    }
+
+    return 0;
 }
