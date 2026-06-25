9d47e930f31dfb81b784d5bfe94ec4d49f9bb1a2
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-25 21:37:06 +0530

    Update Chef and Donation.cpp

--- Chef and Donation.cpp
+++ Chef and Donation.cpp
@@ -1,21 +1,28 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DNATION
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DNATION
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = current donations, Y = required donations
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Additional donations needed
-	    cout << Y - X << endl;
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = current donations collected
+        // Y = target donations required
+        int X, Y;
+        cin >> X >> Y;
+
+        // Calculate the additional donations needed
+        cout << Y - X << endl;
+    }
+
+    return 0;
 }
