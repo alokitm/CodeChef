4546676893ae0dd8f2fdfbabe9d52234e26c186a
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-26 22:45:39 +0530

    Update Chess Time.cpp

--- Chess Time.cpp
+++ Chess Time.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHESSTIME
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CHESSTIME
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
