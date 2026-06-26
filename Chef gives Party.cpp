2552774545c72f88fbb67f7f72e48fb79a558d98
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-26 22:44:45 +0530

    Update Chef gives Party.cpp

--- Chef gives Party.cpp
+++ Chef gives Party.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARTY2
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARTY2
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
