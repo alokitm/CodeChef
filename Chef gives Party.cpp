c2015772861cafd7e2d336f3b668809548838e8f
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-26 22:44:21 +0530

    Update Chef gives Party.cpp

--- Chef gives Party.cpp
+++ Chef gives Party.cpp
@@ -1,28 +1,38 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARTY2
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PARTY2
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// t = number of test cases
-	int t;
-	cin >> t;
 
-	while (t--) {
-	    // n = number of friends
-	    // x = cost per person
-	    // k = total budget
-	    int n, x, k;
-	    cin >> n >> x >> k;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if budget is sufficient
-	    if (k >= n * x) {
-	        cout << "yes" << endl;
-	    }
-	    else {
-	        cout << "no" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = number of friends
+        // X = cost per person
+        // K = total budget available
+        int N, X, K;
+        cin >> N >> X >> K;
+
+        // Total amount required = N × X
+        if (K >= N * X) {
+
+            // Budget is sufficient for the party
+            cout << "yes" << endl;
+        }
+        else {
+
+            // Budget is not sufficient for the party
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
 }
