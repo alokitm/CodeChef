03e4fba70903b4ca5fd516bcbb313ee0fc8dac86
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-28 22:19:52 +0530

    Update Clear Day.cpp

--- Clear Day.cpp
+++ Clear Day.cpp
@@ -1,21 +1,23 @@
-// Problem Link:https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// Variables:
-	// X = number of days Chef worked
-	// Y = number of days spent on other activities
-	int X, Y;
 
-	// Input values of X & Y
-	cin >> X;
-	cin >> Y;
+    // X = number of days Chef worked
+    // Y = number of days spent on other activities
+    int X, Y;
 
-	// Total days in a week = 7
-	// Remaining free days = 7 - (X + Y)
-	cout << 7 - (X + Y);
+    // Input values
+    cin >> X >> Y;
 
-	return 0;
+    // Total days in a week = 7
+    // Free days = 7 - (working days + other activity days)
+    cout << 7 - (X + Y);
+
+    return 0;
 }
