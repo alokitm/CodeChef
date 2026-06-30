36df4dc8858845770035a07448ae1957ee8d8408
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-30 19:49:30 +0530

    Update Cricket World Cup Qualifier.cpp

--- Cricket World Cup Qualifier.cpp
+++ Cricket World Cup Qualifier.cpp
@@ -1,19 +1,28 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 
-    int x;          // number of matches won
-    cin >> x;       // input the number of wins
+    // X = number of matches won
+    int X;
+    cin >> X;
 
-    // check if wins are enough to qualify
-    if (x >= 12) {
-        cout << "Yes";   // team qualifies
-    } 
+    // A team qualifies if it wins at least 12 matches
+    if (X >= 12) {
+
+        // Team qualifies
+        cout << "Yes";
+    }
     else {
-        cout << "No";    // team does not qualify
+
+        // Team does not qualify
+        cout << "No";
     }
 
-    return 0;   // program end
+    return 0;
 }
