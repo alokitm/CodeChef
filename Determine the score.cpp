51f7d93db2643415cc9414dfbd85d87a1e053fde
Alokit Mishra <alokitmishra4@gmail.com>
2026-06-30 19:50:42 +0530

    Update Determine the score.cpp

--- Determine the score.cpp
+++ Determine the score.cpp
@@ -1,15 +1,28 @@
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+
+    // T = number of test cases
     int T;
     cin >> T;
 
-    while(T--) {
+    // Process each test case
+    while (T--) {
+
+        // X = total marks of the exam
+        // N = number of correctly answered questions
         int X, N;
         cin >> X >> N;
 
+        // The exam has 10 questions
+        // Marks per question = X / 10
+        // Total score = marks per question × correct answers
         cout << (X / 10) * N << endl;
     }
 
