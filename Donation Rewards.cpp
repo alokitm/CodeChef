ec3ff56f5ae691d42b43ad6570236e3856cf2bc2
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-02 22:47:16 +0530

    Update Donation Rewards.cpp

--- Donation Rewards.cpp
+++ Donation Rewards.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOREWARD
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOREWARD
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
