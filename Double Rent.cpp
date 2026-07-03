41604febc71dfca9adcf82a1a7b20fdcf49d9b9c
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-03 21:38:27 +0530

    Update Double Rent.cpp

--- Double Rent.cpp
+++ Double Rent.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
 
 // Time Complexity: O(1)
 // Space Complexity: O(1)
