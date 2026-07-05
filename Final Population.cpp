191ae80b5a02443949050bd0dbaf328185115681
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-05 18:25:38 +0530

    Refactor population calculation code
    Updated variable names and comments for clarity.

--- Final Population.cpp
+++ Final Population.cpp
@@ -1,23 +1,30 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// t = number of test cases
-	int t;
-	cin >> t;
 
-	while (t--) {
-	    // x = current population
-	    // y = people who left
-	    // z = people who arrived
-	    int x, y, z;
-	    cin >> x >> y >> z;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Final population
-	    cout << x - y + z << endl;
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = current population
+        // Y = number of people who left
+        // Z = number of people who arrived
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        // Final population
+        // = current population - people who left + people who arrived
+        cout << X - Y + Z << endl;
+    }
+
+    return 0;
 }
