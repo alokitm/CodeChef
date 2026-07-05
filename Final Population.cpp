b4dcd4bb1543672b05dc93c7ba3ed3aff93f9ca4
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-05 18:26:16 +0530

    Update Final Population.cpp

--- Final Population.cpp
+++ Final Population.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
