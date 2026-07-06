a60290b9fe05f99cd37b7f18e24410830d77a7a3
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-06 22:30:12 +0530

    Update Four Tickets.cpp

--- Four Tickets.cpp
+++ Four Tickets.cpp
@@ -1,26 +1,37 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FOURTICKETS
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FOURTICKETS
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = cost of one ticket
-	    int X;
-	    cin >> X;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if total cost of 4 tickets is within budget (<= 1000)
-	    if (4 * X <= 1000) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = cost of one ticket
+        int X;
+        cin >> X;
+
+        // Total cost of 4 tickets = 4 × X
+        // Check if it is within the budget of 1000
+        if (4 * X <= 1000) {
+
+            // Tickets can be purchased within budget
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Budget is insufficient
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
