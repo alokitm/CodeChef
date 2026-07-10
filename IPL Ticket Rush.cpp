0a5057717fbae5c70eb14e58aba8591656ef38e1
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-10 21:47:15 +0530

    Update IPL Ticket Rush.cpp

--- IPL Ticket Rush.cpp
+++ IPL Ticket Rush.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/IPLTRSH
+// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/IPLTRSH
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
