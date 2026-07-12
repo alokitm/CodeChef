1a74df1deeafd6423821b868bf0f42a84977293f
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-12 23:23:02 +0530

    Improve structure and comments in main function
    Refactor code for clarity and organization.

--- Just One More Episode.cpp
+++ Just One More Episode.cpp
@@ -1,26 +1,36 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ONEMORE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ONEMORE
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases given
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = number of hours studied by student 
-	    int X;
-	    cin >> X;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if X exceeds 24 hours
-	    if (X > 24) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = number of hours studied by the student
+        int X;
+        cin >> X;
+
+        // Check whether the study hours exceed 24
+        if (X > 24) {
+
+            // More than 24 hours studied
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // 24 hours or less studied
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
