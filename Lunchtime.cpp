b57b8cd22a8bcc781d4a887f99787b9a0322bcbe
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-15 09:35:29 +0530

    Refactor Lunchtime.cpp for better readability
    Updated variable names and comments for clarity.

--- Lunchtime.cpp
+++ Lunchtime.cpp
@@ -1,26 +1,36 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// t = number of test cases given
-	int t;
-	cin >> t;
 
-	while (t--) {
-	    // x = contest rank
-	    int x;
-	    cin >> x;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if rank is between 1 and 4 (inclusive)
-	    if (x >= 1 && x <= 4) {
-	        cout << "yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = contest rank
+        int X;
+        cin >> X;
+
+        // Check whether the rank is between 1 and 4 (inclusive)
+        if (X >= 1 && X <= 4) {
+
+            // Rank qualifies for Long Challenge
+            cout << "yes" << endl;
+        }
+        else {
+
+            // Rank does not qualify
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
