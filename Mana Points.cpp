8fbd0b2d9dfd0f8cc34035c6f1dc01f69d059ebd
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-16 16:42:57 +0530

    Update problem link comment in Mana Points.cpp

--- Mana Points.cpp
+++ Mana Points.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANAPTS
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANAPTS
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
