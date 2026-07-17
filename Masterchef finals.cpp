e4a8c633cf1272806df18386986dca66fb99250f
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-17 20:03:57 +0530

    Update CodeChef problem

--- Masterchef finals.cpp
+++ Masterchef finals.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
