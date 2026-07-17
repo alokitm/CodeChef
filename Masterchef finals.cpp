e55f0c759d50e0f251273d741fd303d129f8b815
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-17 20:03:28 +0530

    Improve code structure and comments in Masterchef finals
    Refactor code for clarity and maintainability.

--- Masterchef finals.cpp
+++ Masterchef finals.cpp
@@ -1,26 +1,36 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = rank
-	    int X;
-	    cin >> X;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if rank is within top 10
-	    if (1 <= X && X <= 10) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = rank obtained
+        int X;
+        cin >> X;
+
+        // Check whether the rank is within the Top 10
+        if (1 <= X && X <= 10) {
+
+            // Rank is in the Top 10
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Rank is outside the Top 10
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
