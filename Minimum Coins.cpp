6df9948fc923a5ed360eb8b14844d9c0aba25702
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-19 19:37:26 +0530

    Update Minimum Coins.cpp

--- Minimum Coins.cpp
+++ Minimum Coins.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
