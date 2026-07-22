8f4139f4505ef5b9fcde365ad76bc42e1586c382
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-22 22:26:57 +0530

    Update comments and formatting in Off BY One.cpp

--- Off BY One.cpp
+++ Off BY One.cpp
@@ -1,20 +1,23 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// A = number of 10-unit items
-	// B = number of 10-unit items
-	int A, B;
 
-	// Input
-	cin >> A >> B;
+    // A = number of items of the first type
+    // B = number of items of the second type
+    int A, B;
 
-	// Calculation:
-	// Total = (A * 10) + (B * 10) + 1
-	cout << A * 10 + B * 10 + 1;
+    // Input values
+    cin >> A >> B;
 
-	return 0;
+    // Each item contributes 10 units
+    // Final total = (A × 10) + (B × 10) + 1
+    cout << A * 10 + B * 10 + 1;
+
+    return 0;
 }
