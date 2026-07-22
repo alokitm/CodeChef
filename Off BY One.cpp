03ce22545c3c36a1ad6b2b96d4d12ec16d3cb0ed
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-22 22:27:27 +0530

    Update problem link comment in Off BY One.cpp

--- Off BY One.cpp
+++ Off BY One.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
 
 // Time Complexity: O(1)
 // Space Complexity: O(1)
