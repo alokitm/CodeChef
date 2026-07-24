8f669bc3361d50df172d3701ba7f5991aafe2dd1
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-24 21:54:15 +0530

    Improve readability and structure of Parity.cpp
    Refactor code for clarity and maintainability, including comments.

--- Parity.cpp
+++ Parity.cpp
@@ -1,26 +1,36 @@
-// CodeChef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // N = given number
-	    int N;
-	    cin >> N;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if N is even
-	    if (N % 2 == 0) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = given number
+        int N;
+        cin >> N;
+
+        // Check whether N is even
+        if (N % 2 == 0) {
+
+            // N is divisible by 2
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // N is not divisible by 2
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
