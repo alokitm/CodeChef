5654308061694f0cc9490e8601754b25d81df8cd
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-25 16:03:19 +0530

    Update problem link comment in Passes for Fair.cpp

--- Passes for Fair.cpp
+++ Passes for Fair.cpp
@@ -1,4 +1,4 @@
-// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FAIRPASS
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FAIRPASS
 
 // Time Complexity: O(T)
 // Space Complexity: O(1)
