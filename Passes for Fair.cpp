b6fe23bc2aa93fc6b9e2d2718e1dce5e2003d611
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-25 16:03:01 +0530

    Refactor variable names and enhance comments
    Updated variable names for clarity and improved comments.

--- Passes for Fair.cpp
+++ Passes for Fair.cpp
@@ -1,26 +1,37 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FAIRPASS
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/FAIRPASS
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// t = number of test cases
-	int t;
-	cin >> t;
 
-	while (t--) {
-	    // n = number of people, k = pass limit
-	    int n, k;
-	    cin >> n >> k;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check eligibility for fair pass
-	    if (n < k) {
-	        cout << "yes" << endl;
-	    }
-	    else {
-	        cout << "no" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = number of people
+        // K = maximum number of people allowed for a fair pass
+        int N, K;
+        cin >> N >> K;
+
+        // Check whether the number of people is less than the pass limit
+        if (N < K) {
+
+            // Eligible for a fair pass
+            cout << "yes" << endl;
+        }
+        else {
+
+            // Not eligible for a fair pass
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
 }
