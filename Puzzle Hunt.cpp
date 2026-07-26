25e58c638f2fefd4c54d127c8fbb14bfb5afddfa
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-26 19:02:38 +0530

    Refactor Puzzle Hunt.cpp for clarity and structure
    Updated comments for clarity and restructured code for better readability.

--- Puzzle Hunt.cpp
+++ Puzzle Hunt.cpp
@@ -1,27 +1,36 @@
-// Problem Link:
-// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // N = number of puzzles solved
-	    int N;
-	    cin >> N;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if N is between 6 and 8 (inclusive)
-	    if (N >= 6 && N <= 8) {
-	        cout << "yes" << endl;
-	    }
-	    else {
-	        cout << "no" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = number of puzzles solved
+        int N;
+        cin >> N;
+
+        // Check whether the number of solved puzzles is between 6 and 8 (inclusive)
+        if (N >= 6 && N <= 8) {
+
+            // Eligible
+            cout << "yes" << endl;
+        }
+        else {
+
+            // Not eligible
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
 }
