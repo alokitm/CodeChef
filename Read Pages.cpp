2231332d3d6583c3d88484c31d044aa2aecf30c3
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-30 19:43:14 +0530

    Refactor Read Pages logic and enhance comments
    Refactored variable names and improved comments for clarity.

--- Read Pages.cpp
+++ Read Pages.cpp
@@ -1,28 +1,39 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/READPAGES
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// t = number of test cases
-	int t;
-	cin >> t;
 
-	while (t--) {
-	    // n = total pages
-	    // x = pages read per day
-	    // y = number of days
-	    int n, x, y;
-	    cin >> n >> x >> y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if total readable pages (x * y) cover n
-	    if (n <= x * y) {
-	        cout << "yes" << endl;
-	    }
-	    else {
-	        cout << "no" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = total number of pages
+        // X = pages read per day
+        // Y = number of days available
+        int N, X, Y;
+        cin >> N >> X >> Y;
+
+        // Calculate the maximum number of pages that can be read
+        // and check whether all pages can be completed
+        if (N <= X * Y) {
+
+            // All pages can be read
+            cout << "yes" << endl;
+        }
+        else {
+
+            // All pages cannot be read
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
 }
