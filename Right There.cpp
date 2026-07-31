590a9cb5e503e28186f4295973e3b1a5c433ca4f
Alokit Mishra <alokitmishra4@gmail.com>
2026-07-31 23:03:49 +0530

    Update Right There.cpp

--- Right There.cpp
+++ Right There.cpp
@@ -1,26 +1,38 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIGHTTHERE
+// CODECHEF Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIGHTTHERE
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // N = required distance, X = current position
-	    int N, X;
-	    cin >> N >> X;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if Chef can reach or has already reached
-	    if (N <= X) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // N = required destination distance
+        // X = Chef's current position
+        int N, X;
+        cin >> N >> X;
+
+        // Check whether Chef has reached
+        // or crossed the required distance
+        if (X >= N) {
+
+            // Chef is already there
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Chef has not reached yet
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
