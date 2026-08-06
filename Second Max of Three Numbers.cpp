75729c403b3d20a9d5be2921040cef4157852683
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-06 22:43:04 +0530

    Refactor comments and variable names in the code
    Updated comments for clarity and changed variable name from 'n' to 'T' for better understanding.

--- Second Max of Three Numbers.cpp
+++ Second Max of Three Numbers.cpp
@@ -1,27 +1,35 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SNDMAX
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/SNDMAX
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // n = number of test cases
-    int n;
-    cin >> n;
 
-    while (n--) {
-        // Array to store 3 numbers
+    // T = number of test cases
+    int T;
+    cin >> T;
+
+    // Process each test case
+    while (T--) {
+
+        // Array to store the three numbers
         int nums[3];
 
-        // Input
+        // Input the three numbers
         cin >> nums[0] >> nums[1] >> nums[2];
 
         // Sort the array in ascending order
-        // After sorting:
-        // nums[0] = smallest
-        // nums[1] = second maximum (middle)
-        // nums[2] = largest
         sort(nums, nums + 3);
 
-        // Output second maximum
+        // After sorting:
+        // nums[0] = smallest element
+        // nums[1] = second largest element
+        // nums[2] = largest element
+
+        // Print the second largest element
         cout << nums[1] << endl;
     }
 
