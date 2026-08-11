f03fc51aec175491d64735eadf7c2c96a0ea19ff
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-11 19:17:25 +0530

    Update The Cheaper Cab.cpp

--- The Cheaper Cab.cpp
+++ The Cheaper Cab.cpp
@@ -1,21 +1,42 @@
-// Codechef Problem Link : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CABS
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    while(t--) {
-        int x, y;
-        cin >> x >> y;
-        
-        if (x < y) {
+
+    // T = number of test cases
+    int T;
+    cin >> T;
+
+    // Process each test case
+    while (T--) {
+
+        // X = value associated with the first option
+        // Y = value associated with the second option
+        int X, Y;
+        cin >> X >> Y;
+
+        // Compare the two values to determine the winner
+        if (X < Y) {
+
+            // First option has the smaller value
             cout << "FIRST" << endl;
-        } else if (x > y) {
+        }
+        else if (X > Y) {
+
+            // Second option has the smaller value
             cout << "SECOND" << endl;
-        } else {
+        }
+        else {
+
+            // Both values are equal, so either option can be chosen
             cout << "ANY" << endl;
         }
     }
+
     return 0;
 }
