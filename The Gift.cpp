08fefb2c6442d567cad91c8c6f57f10d69e908df
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-12 19:38:06 +0530

    Update The Gift.cpp

--- The Gift.cpp
+++ The Gift.cpp
@@ -1,25 +1,33 @@
-// Codechef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// x = current money
-	// n = cost of the gift
-	// m = additional money received
-	int x, n, m;
-	cin >> x >> n >> m;
 
-	// Total available budget
-	int budget = x + m;
+    // X = current amount of money Chef has
+    // N = cost of the gift
+    // M = additional money received
+    int X, N, M;
+    cin >> X >> N >> M;
 
-	// Check if gift can be purchased
-	if (n > budget) {
-	    cout << "no" << endl;
-	}
-	else {
-	    cout << "yes" << endl;
-	}
+    // Calculate the total available budget
+    int budget = X + M;
 
-	return 0;
+    // Check whether Chef has enough money to buy the gift
+    if (N > budget) {
+
+        // Budget is insufficient
+        cout << "no" << endl;
+    }
+    else {
+
+        // Budget is sufficient
+        cout << "yes" << endl;
+    }
+
+    return 0;
 }
