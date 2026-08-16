30524a181acff2300ac3498c9f99aa95e6af6630
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-16 23:13:58 +0530

    Refactor Tom and Jerry Chase.cpp for better readability
    Refactor code structure and improve comments for clarity.

--- Tom and Jerry Chase.cpp
+++ Tom and Jerry Chase.cpp
@@ -1,26 +1,37 @@
 // CodeChef Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/JERRYCHASE
 
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = speed of Tom, Y = speed of Jerry
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Check if Jerry is faster than Tom
-	    if (Y > X) {
-	        cout << "Yes" << endl;
-	    }
-	    else {
-	        cout << "No" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = speed of Tom
+        // Y = speed of Jerry
+        int X, Y;
+        cin >> X >> Y;
+
+        // Check whether Jerry is faster than Tom
+        if (Y > X) {
+
+            // Jerry can catch/escape as per the condition
+            cout << "Yes" << endl;
+        }
+        else {
+
+            // Jerry is not faster than Tom
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
 }
