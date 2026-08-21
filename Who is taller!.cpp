b60ec46476c421bb623648287d6e8f372d28c5f9
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-21 23:43:58 +0530

    Improve structure and comments in height comparison
    Refactor code for clarity and maintainability.

--- Who is taller!.cpp
+++ Who is taller!.cpp
@@ -1,27 +1,38 @@
 // Problem Link:
 // https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TALLER
 
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// T = number of test cases
-	int T;
-	cin >> T;
 
-	while (T--) {
-	    // X = height of A, Y = height of B
-	    int X, Y;
-	    cin >> X >> Y;
+    // T = number of test cases
+    int T;
+    cin >> T;
 
-	    // Compare heights
-	    if (X > Y) {
-	        cout << "A" << endl;
-	    }
-	    else {
-	        cout << "B" << endl;
-	    }
-	}
+    // Process each test case
+    while (T--) {
 
-	return 0;
+        // X = height of person A
+        // Y = height of person B
+        int X, Y;
+        cin >> X >> Y;
+
+        // Compare the heights
+        if (X > Y) {
+
+            // Person A is taller
+            cout << "A" << endl;
+        }
+        else {
+
+            // Person B is taller
+            cout << "B" << endl;
+        }
+    }
+
+    return 0;
 }
