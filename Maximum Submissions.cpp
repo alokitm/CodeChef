e39c1edf8ab532536a1b132fd1d5e5f765177df0
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-23 19:38:17 +0530

    Add Maximum Submissions program

---
+++ Maximum Submissions.cpp
@@ -0,0 +1,17 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MAXIMUMSUBS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while(T--) {
+        int X;
+        cin >> X;
+
+        cout << 2 * X << endl;
+    }
+
+    return 0;
+}
