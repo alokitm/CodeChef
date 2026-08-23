5920291f21f3ba2fad295a0074b797a611649009
Alokit Mishra <alokitmishra4@gmail.com>
2026-08-23 19:39:33 +0530

    Fix formatting and comments in Maximum Submissions.cpp

--- Maximum Submissions.cpp
+++ Maximum Submissions.cpp
@@ -1,15 +1,25 @@
-https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MAXIMUMSUBS
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MAXIMUMSUBS
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+
+    // T = number of test cases
     int T;
     cin >> T;
 
-    while(T--) {
+    // Process each test case
+    while (T--) {
+
+        // X = number of items
         int X;
         cin >> X;
 
+        // Calculate the maximum number of subscriptions
         cout << 2 * X << endl;
     }
 
