983177674aa4e25d6275cced7c71b686c4d4e716
Alokit Mishra <alokitmishra4@gmail.com>
2026-09-03 18:58:27 +0530

    Update Tyre problem.cpp

--- Tyre problem.cpp
+++ Tyre problem.cpp
@@ -1,18 +1,28 @@
-https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TYRE
+// Problem Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TYRE
+
+// Time Complexity: O(T)
+// Space Complexity: O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+
+    // T = number of test cases
     int T;
     cin >> T;
 
-    while(T--) {
+    // Process each test case
+    while (T--) {
+
+        // N = number of cars
+        // M = number of motorcycles
         int N, M;
         cin >> N >> M;
 
+        // Each car needs 4 tyres and each motorcycle needs 2 tyres
         cout << (2 * N) + (4 * M) << endl;
     }
 
     return 0;
 }
-
