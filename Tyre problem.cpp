914faa1e2e7dc2bccf7158a775f1b6de31a1d45c
Alokit Mishra <alokitmishra4@gmail.com>
2026-09-03 18:57:11 +0530

    Add solution for tyre problem in Tyre problem.cpp
    Implement solution for tyre problem calculation.

---
+++ Tyre problem.cpp
@@ -0,0 +1,18 @@
+https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TYRE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while(T--) {
+        int N, M;
+        cin >> N >> M;
+
+        cout << (2 * N) + (4 * M) << endl;
+    }
+
+    return 0;
+}
+
